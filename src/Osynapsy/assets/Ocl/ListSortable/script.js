673195b59e81216764e855905d618c932020a423
Pietro Celeste <p.celeste@osynapsy.org>
2017-02-15 08:55:34 +0100

    Adjust script and css for new lib (html5sortable) cooperation

--- src/Osynapsy/assets/Ocl/ListSortable/script.js
+++ src/Osynapsy/assets/Ocl/ListSortable/script.js
@@ -1,38 +1,18 @@
 OclListSortable = {
     init : function(){        
         $('.osy-listsortable ul,.osy-listsortable-leaf').sortable({
-            items : 'li.row',
-            containment: "parent",
-            afterMove : function(e,o) {                 
-                var ajax = $(o).closest('.osy-listsortable').attr('id');  
-                console.log(o);
-                if (ajax == 'undefined') {
-                    return;
-                }                         
-                var ipar = $(o.item).closest('ul');
-                 //sort = ipar.sortable('serialize',{attribute :'id',connected:false});
-                var sort = '';
-                $(ipar).children().each(function(){
-                    sort += (sort == '' ? '' : '&') + ajax +'[]='+$(this).attr('oid');
-                });
-                console.log(sort);
-                if (sort == '') {
-                    return;
-                }
-                sort += $('form').serialize()+'&ajax='+ajax;
-                $.ajax({ 
-                    data : sort, 
-                    type : 'post', 
-                    success : function(rsp) {
-                        if (rsp !== 'OK') {
-                            alert(rsp);
-                        }
-                    }
-                });
-            }
+            handle : '.osy-listsortable-item'
+        }).bind('sortupdate', function(e, ui) {
+            var lst = $(e.target).closest('.osy-listsortable');           
+            var sort = [];
+            $('.osy-listsortable-item', lst).each(function(){
+                sort.push($(this).data('id'));
+            }); 
+            $('input[name='+lst.attr('id')+']', lst).val(sort.join(','));
+            FormController.execute(lst)       
         });
-    }    
-}
+    }
+};
 
 if (window.FormController) {    
     FormController.register('init','OclListSortable.init',function(){     
