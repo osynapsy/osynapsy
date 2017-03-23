ef5e9c0f5343549e492b1421c4ed18815068daa0
Pietro Celeste <p.celeste@osynapsy.org>
2017-03-23 07:12:54 +0100

    Bugfix - Selected element don't show selected label

--- src/Osynapsy/Bcl/Component/ListBox.php
+++ src/Osynapsy/Bcl/Component/ListBox.php
@@ -10,26 +10,25 @@ class ListBox extends Component
     public $data = array();
     private $hdn;
     private $box;
-    private $list;
     
-    public function __construct($name)
+    public function __construct($id)
     {
         $this->requireJs('/__assets/osynapsy/Bcl/ListBox/script.js');
         $this->requireCss('/__assets/osynapsy/Bcl/ListBox/style.css');
-        parent::__construct('div',$name.'_container');
+        parent::__construct('div', $id.'_container');
         $this->att('class','listbox');
-        $this->hdn = $this->add(new HiddenBox($name));
+        $this->hdn = $this->add(new HiddenBox($id));
         $this->box = $this->add(new Tag('div'))
                           ->att('class','listbox-box'); 
     }
     
     protected function __build_extra__()
     {
-        $list = $this->add(new Tag('ul'))
-                     ->att('class','listbox-list');
+        $list = $this->add(new Tag('ul'));
+        $list->att('class','listbox-list');
         foreach ($this->data as $rec) {
             $selected = '';
-            if (array_key_exists($this->id, $_REQUEST) && ($rec[0] == $_REQUEST[$this->id])) {
+            if (array_key_exists($this->hdn->id, $_REQUEST) && ($rec[0] == $_REQUEST[$this->hdn->id])) {
                 $this->box->set($rec[1]);
                 $selected = ' selected';
             }
