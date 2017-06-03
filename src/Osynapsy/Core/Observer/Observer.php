d8be6af1a1f728273a7ecffdda6ee595a4238e22
Pietro Celeste <p.celeste@osynapsy.org>
2017-06-03 16:52:44 +0200

    Added observer minimal class implementations

---
+++ src/Osynapsy/Core/Observer/Observer.php
@@ -0,0 +1,17 @@
+<?php
+namespace Osynapsy\Core\Observer;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Observer implements \SplObserver
+{
+    protected $controller;
+    
+    public function update(\SplSubject $controller)
+    {
+        $this->controller = $controller;
+    }
+}
