bdd7299467a9513c5684604c16c6aaf4762a9d9d
Pietro Celeste <p.celeste@osynapsy.org>
2017-10-21 14:29:47 +0200

    Added checkbox BCL component

---
+++ src/Osynapsy/Bcl/Component/CheckBox.php
@@ -0,0 +1,37 @@
+<?php
+namespace Osynapsy\Bcl\Component;
+
+use Osynapsy\Ocl\Component\Component;
+use Osynapsy\Ocl\Component\HiddenBox;
+use Osynapsy\Core\Lib\Tag;
+
+/**
+ * Description of CheckBox
+ *
+ * @author Pietro Celeste <p.celeste@spinit.it>
+ */
+class CheckBox extends Component
+{
+    private $checkbox;
+    private $hidden;
+    
+    public function __construct($id, $label, $value='1')
+    {
+        parent::__construct('label', $id.'_parent');
+        $this->att('class','form-check-label');        
+        $this->hidden = $this->add(new HiddenBox($id))->att('value', '');
+        $this->checkbox = $this->add(new Tag('input'))->att([
+            'id' => $id,
+            'type' => 'checkbox',
+            'name' => $id,
+            'value' => $value
+        ]);
+        $this->add(' '.$label);
+    }
+    
+    public function getCheckBox()
+    {
+        return $this->checkbox;
+    }
+}
+
