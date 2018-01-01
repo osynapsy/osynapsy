057a803aed8434523a88e33701e423c1808a5924
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-01 12:17:27 +0100

    Added new model based on ActiveRecord implementation

---
+++ src/Osynapsy/Core/Model/ModelRecord.php
@@ -0,0 +1,295 @@
+<?php
+namespace Osynapsy\Core\Model;
+
+use Osynapsy\Core\Lib\Dictionary;
+use Osynapsy\Core\Model\ModelField;
+use Osynapsy\Core\Helper\UploadManager;
+
+abstract class ModelRecord
+{
+    private $repo;
+    private $record;
+    protected $controller = null;    
+    protected $db = null;
+    protected $keys = [];
+    protected $softdelete;
+    protected $errorMessages = array(        
+        'email' => 'Il campo <fieldname> non contiene un indirizzo mail valido.',
+        'fixlength' => 'Il campo <fieldname> solo valori con lunghezza pari a ',
+        'integer' => 'Il campo <fieldname> accetta solo numeri interi.',
+        'maxlength' => 'Il campo <fieldname> accetta massimo ',
+        'minlength' => 'Il campo <fieldname> accetta minimo ',
+        'notnull' => 'Il campo <fieldname> è obbligatorio.',
+        'numeric' => 'Il campo <fieldname> accetta solo valori numerici.',
+        'unique' => '<value> è già presente in archivio.'
+    );    
+    
+    public function __construct($controller, $record)
+    {
+        $this->controller = $controller;
+        $this->record = $record;
+        $this->db = $this->controller->getDb();
+        $this->repo = new Dictionary();
+        $this->repo->set('actions.after-insert', $this->controller->request->get('page.url'))
+                   ->set('actions.after-update', 'back')
+                   ->set('actions.after-delete', 'back')
+                   ->set('fields',[])                   
+                   ->set('values',[]);
+        $this->init();
+        $this->find();        
+    }       
+    
+    public function get($key)
+    {
+        return $this->repo->get($key);
+    }
+    
+    public function getRecord()
+    {
+        return $this->record;
+    }
+    
+    public function set($key, $value)
+    {
+        $this->repo->set($key, $value);
+        return $this;
+    }
+       
+    public function delete()
+    {
+        $this->beforeDelete();
+        if ($this->controller->response->error()){ 
+            return; 
+        }
+        $this->getRecord()->delete();
+        $this->afterDelete();
+        if ($this->repo->get('actions.after-delete') === false) {
+            return;
+        }
+        $this->controller->response->go($this->repo->get('actions.after-delete'));
+    }
+
+    public function find()
+    {        
+        try {
+            $this->getRecord()->findByKeys($this->keys);
+        } catch (\Exception $e) {
+            $this->controller->response->addContent(
+                'MODEL FIND ERROR: <pre>'.$e->getMessage()."\n".$sql.'</pre>'
+            );
+        }
+    }
+    
+    public function insert()
+    {
+        $this->beforeInsert();        
+
+        if ($this->controller->response->error()) {
+            return;
+        }        
+
+        $lastId = $this->getRecord()->save();        
+
+        $this->afterInsert($lastId);
+        
+        switch ($this->repo->get('actions.after-insert')) {
+            case false:
+                return;
+            case 'back':
+            case 'refresh':
+                $this->controller->response->go($this->repo->get('actions.after-insert'));                
+                break;
+            default: 
+                $this->controller->response->go($this->repo->get('actions.after-insert').$lastId);                
+                break;
+        }
+    }
+
+    public function update()
+    {
+        $this->beforeUpdate();
+        if ($this->controller->response->error()) {
+            return;
+        }
+        $id = $this->getRecord()->save();   
+        $this->afterUpdate($id);
+        if ($this->repo->get('actions.after-update') === false) {
+            return;
+        }
+        $this->controller->response->go($this->repo->get('actions.after-update'), false);        
+    }    
+        
+    protected function addError($errorId, $field, $postfix = '')
+    {
+        $error = str_replace(
+            array('<fieldname>', '<value>'),
+            array('<!--'.$field->html.'-->', $field->value),            
+            $this->errorMessages[$errorId].$postfix
+        );
+        $this->controller->response->error($field->html, $error);
+    }
+    
+    public function getValue($key)
+    {
+        return $this->get('databaseValues.'.$key);
+    }
+    
+    public function map($htmlField, $dbField = null, $value = null, $type = 'string')
+    {
+        $modelField = new ModelField($this, $dbField, $htmlField, $type);
+        $modelField->setValue($_REQUEST[$modelField->html], $value);
+        $this->set('fields.'.$modelField->html, $modelField);
+        if ($modelField->isPkey()) {
+            $this->keys[$dbField] = $modelField->getDefaultValue();
+        }
+        return $modelField;
+    }
+    
+    /**
+     * 
+     * @return void
+     */
+    public function save()
+    {
+        //Recall before exec method with arbirtary code
+        $this->beforeExec();
+                                     
+        //skim the field list for check value and build $values, $where and $key list
+        foreach ($this->repo->get('fields') as $field) {
+            //Check if value respect rule
+            $value = $this->sanitizeFieldValue($field);
+            //If field isn't in readonly mode assign values to values list for store it in db
+            if (!$field->readonly) {
+                $this->getRecord()->setValue($field->name, $value);
+            }                        
+        }
+        //If occurred some error stop db updating
+        if ($this->controller->response->error()) { 
+            return; 
+        }
+        //If where list is empty execute db insert else execute a db update
+        if ($this->getRecord()->getState() == 'insert') {
+            $this->insert();
+        } else {
+            $this->update();
+        }
+        //Recall after exec method with arbirtary code
+        $this->afterExec();
+    }
+    
+    private function sanitizeFieldValue(&$field)
+    {
+        $value = $field->value;
+        if (!$field->isNullable() && $value !== '0' && empty($value)) {
+            $this->addError('notnull', $field);            
+        }
+        if ($field->isUnique() && $value) {
+            $nOccurence = $this->db->execUnique(
+                "SELECT COUNT(*) FROM {$this->table} WHERE {$field->name} = ?",
+                array($value)
+            );
+            if (!empty($nOccurence)) {
+                $this->addError('unique', $field);
+            }
+        }
+        //Controllo la lunghezza massima della stringa. Se impostata.
+        if ($field->maxlength && (strlen($value) > $field->maxlength)) {
+            $this->addError('maxlength', $field, $field->maxlength.' caratteri');           
+        } elseif ($field->minlength && (strlen($value) < $field->minlength)) {
+            $this->addError('minlength', $field, $field->minlength.' caratteri');
+        } elseif ($field->fixlength && !in_array(strlen($value),$field->fixlength)) {
+            $this->addError('fixlength', $field, implode(' o ',$field->fixlength).' caratteri');            
+        }
+        switch ($field->type) {
+            case 'float':
+            case 'money':
+            case 'numeric':
+            case 'number':
+                if ($value && filter_var($value, \FILTER_VALIDATE_FLOAT) === false) {
+                    $this->addError('numeric', $field);                    
+                }
+                break;
+            case 'integer':
+            case 'int':
+                if ($value && filter_var($value, \FILTER_VALIDATE_INT) === false) {
+                    $this->addError('integer', $field);                    
+                }
+                break;
+            case 'email':
+                if (!empty($value) && filter_var($value, \FILTER_VALIDATE_EMAIL) === false) {
+                    $this->addError('email', $field);                    
+                }
+                break;
+            case 'file':
+            case 'image':                
+                $value = $this->grabUploadedFile($field);                
+                break;
+        }
+        return $value;
+    }
+    
+    private function grabUploadedFile(&$field)
+    {
+        if (
+            !is_array($_FILES) 
+            || !array_key_exists($field->html, $_FILES)
+            || empty($_FILES[$field->html]['name'])
+        ) {
+            $field->readonly = true;            
+            return $field->value;
+        }
+                
+        $upload = new UploadManager();
+        try {
+            $field->value = $upload->saveFile($field->html, $field->uploadDir);
+        } catch(\Exception $e) {
+            $this->controller->response->error('alert', $e->getMessage());
+            $field->readonly = true;            
+        }
+        $this->afterUpload($this->value, $field);
+        return $field->value;
+    }
+    
+    protected function afterUpload($filename, $field = null)
+    {        
+    }
+    
+    public function softDelete($field, $value)
+    {
+        $this->softdelete = array($field => $value);
+    }
+    
+    protected function beforeExec()
+    {
+    }
+    
+    protected function beforeInsert()
+    {
+    }
+    
+    protected function beforeUpdate()
+    {
+    }
+    
+    protected function beforeDelete()
+    {
+    }
+    
+    protected function afterExec()
+    {
+    }
+    
+    protected function afterInsert()
+    {
+    }
+    
+    protected function afterUpdate()
+    {
+    }
+    
+    protected function afterDelete()
+    {
+    }
+    
+    abstract protected function init();        
+}
