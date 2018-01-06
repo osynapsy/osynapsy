1b8b3adc6f10e34cea7d1217beae6053012df48d
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-06 11:36:08 +0100

    Clean code e method inutilized

--- src/Osynapsy/Ocl/Component/ComboBox.php
+++ src/Osynapsy/Ocl/Component/ComboBox.php
@@ -5,12 +5,12 @@ use Osynapsy\Core\Lib\Tag as Tag;
 
 //costruttore del combo box
 class ComboBox extends Component
-{
-    public $__dat = array();
+{    
     public $__grp = array();
     public $isTree = false;
-    public $dba = null;
     public $placeholder = '- Seleziona -';
+    protected $defaultValue;
+    protected $currentValue;
     
     public function __construct($name)
     {
@@ -19,56 +19,35 @@ class ComboBox extends Component
     }
     
     protected function __build_extra__()
-    {
-        if (empty($this->__dat) && $sql = $this->get_par('datasource-sql')) {
-            try {
-                $this->__dat = $this->dba->execQuery(
-                    $sql,
-                    $this->get_par('datasource-sql-par'), 
-                    'BOTH'
-                );
-            } catch(\Exception $e) {
-                $this->att(0,'dummy');
-                $this->add('<div class="osy-error" id="'.$this->id.'">SQL ERROR - [LABEL]</div>');
-                $this->add('<div class="osy-error-msg">'.($e->getMessage()).'<br>'.nl2br($sql).'</div>');
-                return;
-            }
+    {        
+        $this->currentValue = $this->getGlobal($this->name, $_REQUEST);
+        if (!$this->currentValue) {
+            $this->currentValue = $this->defaultValue;
         }
-        if (!empty($this->__dat) && $this->isTree && array_key_exists(2,$this->__dat[0])) {
+        if (!empty($this->data) && $this->isTree && array_key_exists(2,$this->data[0])) {
             if (!$this->get_par('option-select-disable')){
-                array_unshift($this->__dat, array('','- select -','_group'=>''));                
+                array_unshift($this->data, array('','- select -','_group'=>''));                
             }
-            $this->buildTree($this->__dat);
+            $this->buildTree($this->data);
             return;
         } 
         if (!$this->get_par('option-select-disable')){ 
             if ($lbl = $this->get_par('label-inside')){
                 $this->placeholder = $lbl;
             }
-            array_unshift($this->__dat, array('', $this->placeholder)); 
-        }
-        $val = $this->getGlobal($this->name, $_REQUEST);
-        $idx = array(0,1);
-        if ($this->get_par('fields-order')) {
-            $idx = explode(',', $this->get_par('fields-order'));
-        }
-        foreach ($this->__dat as $k => $itm) {
-            $sel = ($val == $itm[$idx[0]]) ? ' selected' : '';
-            $opt = $this->add(new Tag('option'))->att('value',$itm[$idx[0]]);
-            $opt->add($this->nvl($itm[$idx[1]],$itm[$idx[0]]));
-            if ($val == $itm[$idx[0]]){
-                $opt->att('selected','selected');
-            }            
+            array_unshift($this->data, array('', $this->placeholder)); 
+        }                     
+        foreach ($this->data as $k => $item) {
+            $this->addOption($item[0], $item[1]);            
         }
     }
     
-    public function addOption($opt_val,$opt_lbl)
-    {
-        $cmp_val = $this->getGlobal($this->name, $_REQUEST);        
-        $opt = $this->add(tag::create('option'))->att('value',$opt_val);
-        $opt->add(nvl($opt_lbl,$opt_val));
-        if ($cmp_val == $opt_val) {
-            $opt->att('selected','selected');
+    public function addOption($optionValue, $label)
+    {               
+        $option = $this->add(new Tag('option'))->att('value', $optionValue);
+        $option->add($this->nvl($label, $optionValue));
+        if ($this->currentValue == $optionValue) {
+            $option->att('selected','selected');
         }
     }
     
@@ -87,23 +66,24 @@ class ComboBox extends Component
 
     private function buildBranch($dat, $lev = 0)
     {
-        if (empty($dat)) return;
-        $len = count($dat)-1;
-        $cur_val = $this->getGlobal($this->name, $_REQUEST);
+        if (empty($dat)) {
+            return;
+        }
+        $len = count($dat) - 1;        
         foreach ($dat as $k => $rec) {
             $val = array();
             foreach ($rec as $j => $v) {
-                if (!is_numeric($j)) continue;
-                if (count($val) == 2) continue;
-                $sta = (empty($lev)) ? '' : '|';
-                $end = $len == $k    ? "\\" : "|";
+                if (!is_numeric($j)) {
+                    continue;
+                } elseif (count($val) == 2) {
+                    continue;
+                }               
                 $val[] = empty($val) ? $v : str_repeat('&nbsp;',$lev*5).$v;
-            }
-            $sel = ($cur_val == $val[0]) ? ' selected' : '';
-            $opt = $this->add(tag::create('option'))
+            }           
+            $opt = $this->add(new Tag)
                         ->att('value',$val[0]);
             $opt->add($this->nvl($val[1],$val[0]));
-            if ($cur_val == $val[0]) {
+            if ($this->currentValue == $val[0]) {
                 $opt->att('selected','selected');
             }
             //$this->add('<option value="'.$val[0].'"'.$sel.'>'.nvl($val[1],$val[0])."</option>\n");
@@ -113,41 +93,20 @@ class ComboBox extends Component
         }
     }
     
-    public function setDbHandler($db)
-    {
-        $this->db = $db;
-    }
-    
-    public function setDatasource($source, $db = null)
-    {
-        if (empty($db)){
-            $trasform = array();
-            if (is_array($source)){
-                foreach($source as $k => $v) {
-                    $trasform[] = [0 => $k, 1 => $v];
-                }
-                $source = $trasform;
-            }
-            $this->__dat = $source;
-            return $this;
-        }
-        $this->__par['datasource-sql'] = $source;
-        $this->dba = $db;
-        return $this;
-    }
-    
     public function setArray($array)
     {
-        $this->__dat = $array;
-    }
-    
-    public function setData($data)
-    {
-        $this->__dat = $data;
+        $this->data = $array;
+        return $this;
     }
 
-    public function setTree($active=true)
+    public function setTree($active = true)
     {
         $this->isTree = $active;
+    }
+    
+    public function setDefaultValue($value)
+    {
+        $this->defaultValue = $value;
+        return $this;
     }
 }
