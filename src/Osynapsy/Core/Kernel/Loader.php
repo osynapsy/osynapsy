a122e4d854046d4f4a36bbaf68449ce16928a150
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-14 12:27:41 +0100

    Implements Kernel Module

---
+++ src/Osynapsy/Core/Kernel/Loader.php
@@ -0,0 +1,99 @@
+<?php
+namespace Osynapsy\Core\Kernel;
+
+use Osynapsy\Core\Lib\Dictionary;
+
+/**
+ * Description of LoaderXml
+ *
+ * @author Pietro Celeste <p.celeste@osynapsy.org>
+ */
+class Loader
+{    
+    private $repo;
+    private $path;
+    
+    public function __construct($path)
+    {            
+        $this->path = $path;
+        $this->repo = new Dictionary();
+        $this->repo->set('configuration', $this->load());
+        $this->loadAppConfiguration();
+    }
+    
+    private function loadDir($path)
+    {
+        return [];
+    }
+
+    private function loadFile($path)
+    {
+        $xml = new \SimpleXMLIterator($path,null,true);
+        return $this->parseXml($xml);
+    }
+    
+    private function loadAppConfiguration()
+    {
+        $apps = $this->repo->get('configuration.app');
+        foreach(array_keys($apps) as $app) {
+            $path = '../vendor/'.str_replace("_", "/", $app).'/etc/config.xml';
+            if (is_file($path)) {
+                $this->repo->append('configuration.app.'.$app, $this->loadFile($path));
+            }
+        }
+    }
+    
+    private function parseXml($xml, &$array = [])
+    {                                
+        for($xml->rewind(); $xml->valid(); $xml->next() ) {
+            $key = $xml->key();
+            if (!array_key_exists($key, $array)) {
+                $array[$key] = [];
+            }
+            if ($xml->hasChildren()){
+                $this->parseXml($xml->current(), $array[$key]);
+                continue;
+            }
+            
+            $raw = (array) $xml->current()->attributes();
+            if (empty($raw)) {
+               $array[$key] =  trim(strval($xml->current()));
+               continue;
+            }
+            $element = [
+                $key.'Value' => trim(strval($xml->current()))
+            ];
+            $element += $raw['@attributes'];
+            $array[$key][] = $element;
+        }
+        return $array;
+    }
+    
+    private function load()
+    {    
+        $array = [];
+        if (is_file($this->path)) {
+            $array = $this->loadFile($this->path);
+        } elseif (is_dir($this->path)) {
+            $array = $this->loadDir();        
+        }
+        return $array;
+    }
+    
+    public function get($key = '')
+    {
+        return $this->repo->get('configuration'.(empty($key) ? '' : ".{$key}"));
+    }
+            
+    public function search($keySearch, $searchPath = null, $debug = false)
+    {
+        $fullPath = 'configuration';
+        if (!empty($searchPath)) {
+            $fullPath .= '.'.$searchPath;
+        }
+        if ($debug) {
+            var_dump($fullPath);
+        }
+        return $this->repo->search($keySearch, $fullPath);
+    }
+}
---
+++ src/Osynapsy/Core/Kernel/Runner.php
@@ -0,0 +1,152 @@
+<?php
+namespace Osynapsy\Core\Kernel;
+
+use Osynapsy\Core\Lib\Dictionary;
+use Osynapsy\Core\Network\Router;
+use Osynapsy\Core\Driver\DbPdo;
+use Osynapsy\Core\Driver\DbOci;
+
+/**
+ * Description of Runner
+ *
+ * @author Pietro Celeste <p.celeste@spinit.it>
+ */
+class Runner
+{
+    private $env;
+    private $router;
+    private $applicationId;
+    private $databaseConnections = [];
+    
+    public function __construct(Dictionary &$env, Router $router)
+    {
+        $this->env = $env;
+        $this->router = $router;
+        $this->route = $this->router->getRoute();        
+    }
+    
+    private function checks()
+    {
+        if (empty($this->route)) {
+            throw new \Exception('No route to destination', 404);
+        }
+        if (empty($this->route['application'])) {
+            throw new \Exception('No application defined', 405);
+        }
+        $this->applicationId = $this->route['application'];
+    }
+    
+    public function run()
+    {
+        try {
+            $this->checks();        
+            $this->loadDatasources();
+            $this->runApplicationController();
+            $response = $this->runRouteController(
+                $this->router->getRoute('controller')
+            );
+            if ($response !== false) {
+                return $response;
+            }
+        } catch (\Exception $e) {
+            switch($e->getCode()) {
+                case '404':
+                    return $this->pageNotFound();
+                default :
+                    return $this->pageOops($e->getMessage(), $e->getTrace());                 
+            }            
+        }        
+    }
+    
+    private function runApplicationController()
+    {        
+        $applicationController = str_replace(':', '\\', $this->env->get("env.app.{$this->applicationId}.controller"));
+        if (empty($applicationController)) {
+            return true;
+        }
+        //If app has applicationController instance it before recall route controller;        
+        $this->appController = new $applicationController(
+            array_values($this->databaseConnections)[0], 
+            $this->router->getRoute()
+        );
+        if (!$this->appController->run()) {
+            throw \Exception('App not running (access denied)','501');
+        }
+    }
+    
+    private function runRouteController($classController)
+    {
+        if (empty($classController)) {
+            throw \Exception('Route not found', '404');
+        }
+        $this->controller = new $classController(
+            $this->env, 
+            array_values($this->databaseConnections)[0], 
+            $this->appController
+        );
+        return (string) $this->controller->run();
+    }
+    
+    private function loadDatasources()
+    {            
+        $listDatasource = $this->env->search('db',"env.app.{$this->applicationId}.datasources");
+        foreach ($listDatasource as $datasource) {
+            $connectionStr = $datasource['dbValue'];
+            $connectionSha1 = sha1($connectionStr);
+            if (array_key_exists($connectionSha1, $this->databaseConnections)) {
+                continue;
+            }
+            $this->databaseConnections[$connectionSha1] = $this->getDatabaseConnection($connectionStr);               
+            $this->databaseConnections[$connectionSha1]->connect();                        
+        }
+    }
+    
+    private function getDatabaseConnection($connectionString)
+    {        
+        if (strpos($connectionString, 'oracle') !== false) {
+            return new DbOci($connectionString);
+        } 
+        return new DbPdo($connectionString);
+    }
+    
+    public function pageNotFound($message = 'Page not found')
+    {
+        ob_clean();
+        header('HTTP/1.1 404 Not Found');
+        return $message;
+    }
+    
+    public function pageOops($message, $trace)
+    {
+        ob_clean();
+        $body = '';
+        foreach ($trace as $step) {
+            $body .= '<tr>';
+            $body .= '<td>'.$step['class'].'</td>';
+            $body .= '<td>'.$step['function'].'</td>';
+            $body .= '<td>'.$step['file'].'</td>';
+            $body .= '<td>'.$step['line'].'</td>';            
+            $body .= '</tr>';            
+        }
+        return <<<PAGE
+            <style>
+                * {font-family: Arial;} 
+                div.container {margin: auto;} 
+                td,th {font-size: 12px; font-family: Arial; padding: 3px; border: 0.5px solid silver}
+            </style>
+            <div class="container">       
+                {$message}
+                <table style="border-collapse: collapse; max-width: 1200px;">
+                    <tr>
+                        <th>Class</th>
+                        <th>Function</th>
+                        <th>File</th>
+                        <th>Line</th>
+                    </tr>
+                    {$body}
+                </table>
+            </div>
+PAGE;
+                    
+    }
+}
