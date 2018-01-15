22d99fa91463e6f9f0d1d87413764ec5ba10f95a
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-15 11:58:55 +0100

    Arrange namespace

--- src/Osynapsy/Core/Data/Driver/DbOci.php
+++ src/Osynapsy/Core/Data/Driver/DbOci.php
@@ -10,7 +10,7 @@
  * @license  GPL http://www.gnu.org/licenses/gpl-3.0.en.html
  * @link     http://docs.osynapsy.org/ref/DbOci
  */
-namespace Osynapsy\Core\Driver;
+namespace Osynapsy\Core\Data\Driver;
 
 class DbOci implements InterfaceDbo
 {
