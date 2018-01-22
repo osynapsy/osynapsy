405b999b320262b2515447d6b9ceff9f2fcd200c
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-22 16:07:35 +0100

    Moved into Osynapsy\Helper\Language package

---
+++ src/Osynapsy/Helper/Language/JQuery.php
@@ -0,0 +1,38 @@
+<?php
+namespace Osynapsy\Helper\Language;
+
+/**
+ * Description of JQuery
+ *
+ * @author Pietro Celeste
+ */
+class JQuery
+{
+    private $elements = array();
+    private $selector = '';
+    
+    public function __construct($selector)
+    {
+        $this->selector = $selector;
+    }
+    
+    public function __call($method, $params)
+    {
+        $this->elements[$method] = $params;
+        return $this;
+    }
+    
+    public function __toString()
+    {
+        $string = '$(\''.$this->selector.'\')';
+        foreach ($this->elements as $method => $params) {
+            $string .= '.'.$method.'(';
+            foreach ($params as $i => $par) {
+                $string .= empty($i) ? '' : ',';
+                $string .= is_string($par) ? '\''.addslashes($par).'\'' : $par;
+            }
+            $string .= ')';
+        }
+        return $string;
+    }
+}
