4387f82239ec3a7442604b0400fb787565096e21
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-22 18:51:58 +0100

    Upgraded InterfaceController

--- src/Osynapsy/Core/Controller/InterfaceController.php
+++ src/Osynapsy/Core/Controller/InterfaceController.php
@@ -1,12 +1,12 @@
 <?php
 namespace Osynapsy\Core\Controller;
 
-use Osynapsy\Core\Network\Request;
+use Osynapsy\Core\Http\Request\Request;
 use Osynapsy\Core\Data\Driver\DbFactory;
 
 interface InterfaceController
 {
-    public function __construct(Request $request = null, DbFactory $db = null, $appController = null);
+    public function __construct(Request $request = null, DbFactory $db = null, InterfaceApplication $appController = null);
     
     public function getResponse();
 }
