42dd10d2471ed6352053bef494c9be2f7b5521b4
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-22 16:07:48 +0100

    Moved into Osynapsy\Helper\Language package

---
+++ src/Osynapsy/Helper/Language/Sql.php
@@ -0,0 +1,143 @@
+<?php
+namespace Osynapsy\Helper\Language;
+
+/**
+ * Description of Sql
+ *
+ * @author Peter
+ */
+class Sql {
+    //put your code here
+    private $debug;
+    private $part = [
+        'SELECT' => ['separator' => ','.PHP_EOL],
+        'JOIN'   => ['separator' => ' '.PHP_EOL,'postfix' => PHP_EOL],
+        'WHERE'  => ['separator' => ' '],
+        'ORDER BY' => ['separator' => ','.PHP_EOL]
+    ];
+    private $elements = [
+        'SELECT' => array(),
+        'FROM' => array(),
+        'JOIN' => array(),
+        'WHERE' => array(),
+        'ORDER BY' => array()
+    ];
+    private $parameters = [];
+    
+    public function __construct($debug = false)
+    {
+        $this->debug = $debug;
+    }
+    
+    public function condition($condition, callable $function)
+    {
+        if (!$condition) {
+            return $this;
+        }
+        $function($this);
+        return $this;
+    }
+    
+    public function select(array $fields = null)
+    {
+        if (empty($fields)) {
+            return;
+        }        
+        if (!is_array($fields)) {
+            $fields = array($fields);
+        } elseif ($this->isAssoc($fields)) {
+            $app = array();
+            foreach($fields as $key => $value) {
+                $app[] = $value.' as "'.$key.'"';
+            }            
+            $fields = $app;
+        }
+        $this->elements['SELECT'] = array_merge($this->elements['SELECT'], $fields);
+        return $this;
+    }
+    
+    public function from($table, $fields = null)
+    {
+        $this->select($fields);
+        $this->elements['FROM'] = $table;
+        return $this;
+    }
+    
+    public function join($table, array $on, array $fields = null)
+    {
+        $this->select($fields);
+        $this->elements['JOIN'][] = 'INNER JOIN '.$table;
+        $this->elements['JOIN'][] = 'ON ('.implode(' AND ',$on).')';
+        return $this;
+    }
+    
+    public function joinLeft($table, array $on, array $fields = null)
+    {
+        $this->select($fields);
+        $this->elements['JOIN'][] = 'LEFT JOIN '.$table;
+        $this->elements['JOIN'][] = 'ON ('.implode(' AND ',$on).')';
+        return $this;
+    }
+    
+    public function where($condition, array $parameters = array(), $operator = 'AND')
+    {
+        if (!empty($this->elements['WHERE'])) {
+            $condition = $operator . ' ' . $condition;
+        } 
+        $this->elements['WHERE'][] = $condition;
+        $this->parameters = array_merge(
+            $this->parameters,
+            is_array($parameters) ? $parameters : [$parameters]
+        );
+        return $this;
+    }
+    
+    public function orderBy(array $fields)
+    {
+        if (empty($fields)) {
+            return;
+        } elseif (!is_array($fields)) {
+            $fields = array($fields);
+        }
+        $this->elements['ORDER BY'] = array_merge($this->elements['ORDER BY'], $fields);
+        return $this;
+    }
+    
+    public function __toString() {
+        $string = '';
+        foreach ($this->elements as $word => $items) {            
+            $string .= $word == 'JOIN' ? '' : $word.' ';
+            $string .= $this->prefix($word);            
+            $string .= is_array($items) ? implode($this->getSeparator($word), $items) : ' '.$items;            
+            $string .= $this->postfix($word);            
+            $string .= PHP_EOL;
+        }
+        return $string;
+    }
+    
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+    
+    
+    private function prefix($word)
+    {
+        return array_key_exists($word, $this->part) ? $this->part[$word][0] : ' ';
+    }
+    
+    private function postfix($word)
+    {
+        return array_key_exists($word, $this->part) ? $this->part[$word][1] : ' ';
+    }
+    
+    private function getSeparator($word)
+    {
+        return array_key_exists($word, $this->part) ? $this->part[$word]['separator'] : ' ';
+    }
+    
+    private function isAssoc($array)
+    {
+        return !($array === array_values($array));
+    }
+}
