b2df77e2e32368ce0733d80b98f3edc02ae91070
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-22 19:02:08 +0100

    Nothing

--- src/Osynapsy/Core/Controller/InterfaceController.php
+++ src/Osynapsy/Core/Controller/InterfaceController.php
@@ -6,7 +6,7 @@ use Osynapsy\Core\Data\Driver\DbFactory;
 
 interface InterfaceController
 {
-    public function __construct(Request $request = null, DbFactory $db = null, InterfaceApplication $appController = null);
+    public function __construct(Request $request = null, DbFactory $db = null, $appController = null);
     
     public function getResponse();
 }
