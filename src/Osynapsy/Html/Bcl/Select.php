200f2b8f0ee15e04f6eac2ed5e28f6e0e896b444
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-25 09:44:04 +0100

    Deleted reference to par method

--- src/Osynapsy/Html/Bcl/Select.php
+++ src/Osynapsy/Html/Bcl/Select.php
@@ -18,7 +18,7 @@ class Select extends ComboBox2
         $this->requireCss('/__assets/osynapsy/Lib/bootstrap-select-1.10.0/bootstrap-select.css');
         $this->requireJs('/__assets/osynapsy/Lib/bootstrap-select-1.10.0/bootstrap-select.js');
         $this->requireJs('/__assets/osynapsy/Bcl/Select/script.js');
-        //$this->par('option-select-disable',false);
+        //$this->setParameter('option-select-disable',false);
         if ($multiple) {
             $this->setMultiSelect();
         }
