87e2fa1fbc3ae38c1bc79374d3ddc53af67e8a96
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-26 09:54:54 +0100

    Improve code

--- src/Osynapsy/Html/Bcl/NavBar.php
+++ src/Osynapsy/Html/Bcl/NavBar.php
@@ -28,7 +28,8 @@ class NavBar extends Component
     public function __construct($id)
     {
         parent::__construct('nav', $id.'_navbar');
-        $this->setParameter('containerClass', 'container');        
+        $this->setParameter('containerClass', 'container');
+        $this->setData([],[]);
     }
     
     public function __build_extra__()
@@ -38,7 +39,8 @@ class NavBar extends Component
         $container->att('class', $this->getParameter('containerClass'));
         
         $this->buildBrand($container);
-        $this->buildUlMenu($container, $this->data);
+        $this->buildUlMenu($container, $this->data['primary'])->att('class','nav navbar-nav'); 
+        $this->buildUlMenu($container, $this->data['secondary'])->att('class','nav navbar-nav pull-right');
     }
     
     private function buildBrand($container)
@@ -56,13 +58,11 @@ class NavBar extends Component
     
     private function buildUlMenu($container, $data, $level = 0)
     {
+        $ul = $container->add(new Tag('ul'))
+                        ->att('class', ($level > 0 ? 'dropdown-menu' : ''));
         if (empty($data) || !is_array($data)) {
-            return;
-        }
-        $ul = $container->add(new Tag('ul'));
-        if ($level > 0) {
-            $ul->att('class', 'dropdown-menu');
-        }
+            return $ul;
+        }               
         foreach($data as $label => $menu){
             $li = $ul->add(new Tag('li'));
             if (!is_array($menu)) {
@@ -75,15 +75,25 @@ class NavBar extends Component
                 ->add($label.' <span class="caret"></span>');
             $this->buildUlMenu($li, $menu, $level + 1);
         }
+        return $ul;
     }
     
     public function setContainerFluid($bool = true)
     {
         $this->setParameter('containerClass','container'.($bool ? '-fluid' : ''));
+        return $this;
     }
     
     public function setBrand($label, $href)
     {
         $this->setParameter('brand', [$label, $href]);
+        return $this;
+    }
+    
+    public function setData(array $primary, array $secondary = [])
+    {
+        $this->data['primary'] = $primary;
+        $this->data['secondary'] = $secondary;
+        return $this;
     }
 }
