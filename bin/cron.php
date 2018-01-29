8f78502d6a1c4518be05ed6e3ee0bdf7e29bc538
Pietro Celeste <p.celeste@osynapsy.org>
2018-01-29 20:28:49 +0100

    Added comment

--- bin/cron.php
+++ bin/cron.php
@@ -1,5 +1,13 @@
 <?php
-//var_dump($argv[1]);
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 chdir(__DIR__);
 
