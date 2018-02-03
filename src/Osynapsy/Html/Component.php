622f7049be8dedb897e16b54ec5eecc04cb04593
Pietro Celeste <p.celeste@osynapsy.org>
2018-02-03 12:12:36 +0100

    Added setAction method + comment to methods

--- src/Osynapsy/Html/Component.php
+++ src/Osynapsy/Html/Component.php
@@ -28,22 +28,40 @@ class Component extends Tag
             self::$ids[$id] = $this;
         }
     }
-    
+      
     protected function build()
     {
         $this->__build_extra__();
         return parent::build(-1);
     }
-    
+   
+    /**
+     * Overwrite this method if your component need of extra build operation
+     *      
+     * @return void
+     */ 
     protected function __build_extra__()
     {
     }
-            
+    
+    /**
+     * Return component through his id
+     * 
+     * @param $id name of component to return;
+     * @return object
+     */
     public static function getById($id)
     {
         return array_key_exists($id, self::$ids) ? self::$ids[$id] : null;
     }
     
+    /**
+     * Return value of key from array
+     * 
+     * @param $nam name of value to return
+     * @param $array array where search the value
+     * @return mixed
+     */
     public function getGlobal($nam, $array)
     {    
         if (strpos($nam,'[') === false){
@@ -65,11 +83,22 @@ class Component extends Tag
         return $res;
     }
     
+    /**
+     * Return value of parameter
+     * 
+     * @param $key name of parameter to return;
+     * @return mixed
+     */
     public function getParameter($key)
     {
         return array_key_exists($key, $this->__par) ? $this->__par[$key] : null;
     }
     
+    /**
+     * Return list of required file (css, js etc) for correct initialization of component
+     *      
+     * @return array
+     */
     public static function getRequire()
     {
         return self::$require;
@@ -90,34 +119,89 @@ class Component extends Tag
         }
     }
     
+    /**
+     * Append required js file to list of required file
+     * 
+     * @param $file web path of file;
+     * @return void
+     */    
     public static function requireJs($file)
     {
         self::requireFile($file, 'js');
     }
     
+    /**
+     * Append required js code to list of required initialization
+     * 
+     * @param $code js code to append at html page;
+     * @return void
+     */
     public static function requireJsCode($code)
     {
         self::requireFile($code, 'jscode');
     }
     
+    /**
+     * Append required css to list of required File
+     * 
+     * @param $file web path of css file;
+     * @return void
+     */
     public static function requireCss($file)
     {
         self::requireFile($file, 'css');
     }
     
+    /**
+     * Append css class to component class attribute
+     * 
+     * @param $class name of css class to add;
+     * @return $this
+     */
     public function setClass($class, $append = true)
     {
         return $this->att('class',' '.$class, $append);
     }
     
+    /**
+     * Set data internal property of component
+     * 
+     * @param array $data set of data;     
+     * @return $this
+     */
     public function setData($data)
     {
         $this->data = $data;
         return $this;
     }
     
+    /**
+     * Set value for internal parameter of component
+     * 
+     * @param string $key name of the parameter
+     * @param string $value value to assign parameter
+     * @return $this
+     */
     public function setParameter($key, $value = null)
     {
         $this->__par[$key] = $value;
+        return $this;
+    }
+    
+    /**
+     * Set action to recall via ajax
+     * 
+     * @param string $action name of the action without Action final
+     * @param string $parameters parameters list (comma separated) to pass action
+     * @return $this
+     */
+    public function setAction($action, $parameters = null, $class = 'click-execute')
+    {
+        $this->setClass($class)
+             ->att('data-action',$action);
+        if (!empty($parameters)) {
+            $this->att('data-action-parameters',$parameters);
+        }
+        return $this;
     }
 }
