3dd895b41e80a3d20b93b8145690ad8ec6b08147
Pietro Celeste <p.celeste@osynapsy.org>
2018-02-03 18:34:44 +0100

    Added onblur method

--- assets/Bcl/Summernote/script.js
+++ assets/Bcl/Summernote/script.js
@@ -8,7 +8,31 @@ BclSummernote =
             }
             $(this).summernote({
                 onkeyup: function(e) {
-                    $(".summernote").val($(this).code());
+                    //$(".summernote").val($(this).code());
+                },
+                onblur: function(e) {
+                    var phpkey1 = "&lt;"+"?php",
+                        phpkeyend = "?&gt;",
+                        stylekey1 = "&lt;style&gt;",
+                        stylekey2 = "&lt;style type=\"text/css\"&gt;",
+                        stylekeyend = "&lt;/style&gt;",
+                        scriptkey1 = "&lt;script&gt;",
+                        scriptkey2 = "&lt;script type=\"text/javascript\"&gt;",
+                        scriptkeyend = "&lt;/script&gt;";
+
+                    var code = $(this).code();
+
+                    code = $.trim(code)
+                      .replace(/<!--\?php/g, phpkey1)
+                      .replace(/\?-->/g, phpkeyend)
+                      .replace(/<style>/g, stylekey1)
+                      .replace(/<style type="text\/css">/g, stylekey2)
+                      .replace(/<\/style>/g, stylekeyend)
+                      .replace(/<script>/g, scriptkey1)
+                      .replace(/<script type="text\/javascript">/g, scriptkey2)
+                      .replace(/<\/script>/g, scriptkeyend);
+
+                    var content = $("textarea[name='"+$(this).attr('id')+"']").html(code);
                 },
                 onImageUpload: function(files, editor, welEditable) {
                     BclSummernote.upload(files[0], editor, welEditable);
