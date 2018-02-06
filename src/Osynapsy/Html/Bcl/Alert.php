2c20d38bffd7fdabff2bf16a1009e6cfce77a512
Pietro Celeste <p.celeste@osynapsy.org>
2018-02-06 07:32:43 +0100

    Bugfix

--- src/Osynapsy/Html/Bcl/Alert.php
+++ src/Osynapsy/Html/Bcl/Alert.php
@@ -21,7 +21,7 @@ class Alert extends Component
     public function __construct($id, $value, $type='info')
     {
         parent::__construct('div', $id.'_label');
-        $hiddenBox = $this->add(new HiddenBox($id));
+        $this->hiddenBox = $this->add(new HiddenBox($id));
         $this->att('class','alert alert-'.$type)
              ->att('role','alert')
              ->add($value);
