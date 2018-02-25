ea0a8aaca1e6e2fa372c091fd88a741a1c7f65c0
Pietro Celeste <p.celeste@osynapsy.org>
2018-02-25 21:45:53 +0100

    Add method getFoot and setCommand method

--- src/Osynapsy/Html/Bcl/Container.php
+++ src/Osynapsy/Html/Bcl/Container.php
@@ -16,6 +16,9 @@ use Osynapsy\Html\Tag;
 class Container extends Tag
 {
     private $currentRow;
+    private $foot;
+    private $footLeft;
+    private $footRight;
     
     public function __construct($id, $tag='div')
     {
@@ -25,6 +28,16 @@ class Container extends Tag
         }
     }
 
+    private function getFoot($right = false)
+    {
+        if (empty($this->foot)) {
+            $this->foot = $this->addRow();
+            $this->footLeft = $this->foot->add(new Tag('div'))->att('class', 'col-lg-6');
+            $this->footRight = $this->foot->add(new Tag('div'))->att('class', 'col-lg-6 text-right');
+        }
+        return empty($right) ? $this->footLeft : $this->footRight;        
+    }
+    
     public function AddRow()
     {
         return $this->currentRow = $this->add(new Tag('div'))->att('class','row');
@@ -45,5 +58,29 @@ class Container extends Tag
     {
         $this->AddRow();
         $this->AddColumn(12)->add('<h1>'.$title.'</h1>');
+    }
+    
+    public function setCommand($delete = false, $save = true, $back = true)
+    {
+        if ($delete) {
+            $this->getFoot(true)
+                 ->add(new Button('btn_delete', 'button', 'btn-danger'))
+                 ->setAction('delete')
+                 ->att('data-confirm', 'Sei sicuro di voler eliminare il record corrente?')
+                 ->add('<span class="glyphicon glyphicon-trash"></span> Elimina');
+        }
+        if ($save) {
+            $this->getFoot(true)
+                 ->add(new Button('btn_save', 'button', 'btn-primary'))
+                 ->setAction('save')
+                 ->add($save === true ? '<span class="glyphicon glyphicon-floppy-disk"></span> Salva' : $save);
+        }
+        
+        if ($back) {
+            $this->getFoot()
+                 ->add(new Button('btn_back'))
+                 ->att('class','cmd-back btn btn-default')
+                 ->add('<span class="glyphicon glyphicon-chevron-left"></span> Indietro');
+        }
     }
 }
