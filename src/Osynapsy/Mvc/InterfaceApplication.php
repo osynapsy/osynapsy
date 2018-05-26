21fe2380d4590583b42e98be086b7daaeb9f4706
Pietro Celeste <p.celeste@osynapsy.org>
2018-05-26 13:58:22 +0200

    Pass for reference route parameter

--- src/Osynapsy/Mvc/InterfaceApplication.php
+++ src/Osynapsy/Mvc/InterfaceApplication.php
@@ -15,7 +15,7 @@ use Osynapsy\Kernel\Route;
 
 interface InterfaceApplication
 {
-    public function __construct($db, Route $route);
+    public function __construct($db, Route &$route);
     
     public function run();
 }
