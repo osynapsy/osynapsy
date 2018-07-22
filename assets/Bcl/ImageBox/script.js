6710f343ca36f1bdda4c186fe127c7932ce6088f
Pietro Celeste <p.celeste@osynapsy.org>
2018-07-22 13:41:32 +0200

    Improve code of cropper function

--- assets/Bcl/ImageBox/script.js
+++ assets/Bcl/ImageBox/script.js
@@ -1,4 +1,4 @@
-BclImageBox =
+ImageBox2 =
 {
     init : function ()
     {        
@@ -6,11 +6,10 @@ BclImageBox =
             setTimeout(
                 function(){
                     $('img.imagebox-main').each(function(){
-                        $(this).cropper('reset');
-                        BclImageBox.setCropDimension($(this));                        
+                        ImageBox2.initCropBox(this);
                     });
                 },
-                300
+                500
             );
         });
         $('.osy-imagebox-bcl').on('change','input[type=file]',function(e){
@@ -18,65 +17,78 @@ BclImageBox =
             var m = filepath.match(/([^\/\\]+)$/);
             var filename = m[1];
             $('.osy-imagebox-filename').text(filename);
-            var uploadAction = $(this).closest('.osy-imagebox-bcl').data('action');            
+            //var uploadAction = $(this).closest('.osy-imagebox-bcl').data('action');            
             FormController.execute($(this).closest('.osy-imagebox-bcl'));
         });
-        $('img.imagebox-main').each(function() {
-            var cropWidth = $(this).closest('.crop').data('max-width');
-            var cropHeight = $(this).closest('.crop').data('max-height');
-            $(this).cropper({
-                viewMode : 0,
-                modal: true,
-                dragMode: 'none',
-                cropBoxResizable : false,
-                data : true,
-                zoomOnWheel: false,
-                minCropBoxWidth:cropWidth,
-                minCropBoxHeight:cropHeight,
-                crop: function(e) {
-                    // Output the result data for cropping image.
-                    var imgData = $(this).cropper('getImageData');
-                    var factor = imgData.width / imgData.naturalWidth;
-                    var crpData = imgData.width + ',';
-                        crpData += imgData.height + ',';
-                        crpData += (e.x * factor) + ',';
-                        crpData += (e.y * factor) + ','; 
-                        crpData += (e.width * factor) + ',';
-                        crpData += (e.height * factor);
-                    //$(this).data('action','crop');
-                    $(this).data('action-parameters', crpData);
-                },
-                built : function() {
-                    $(this).cropper('setCropBoxData',{width: cropWidth, height: cropHeight, x:0, y:0});
-                }
-            });                          
-        });
         $('.crop-command').click(function() {
-            var par = $(this).closest('.crop');
-            FormController.execute($('img.imagebox-main', par));            
+            ImageBox2.crop(this);
         });
         $('.zoomin-command').click(function(){
-            var par = $(this).closest('.crop');
-            $('img.imagebox-main', par).cropper('zoom',0.1);            
+            var parent = $(this).closest('.crop');
+            $('img.imagebox-main', parent).cropper('zoom',0.05);
+            var zoom = $(parent).data('zoom') + 0.05;
+            $(parent).data('zoom',zoom);
+            //ImageBox2.setCropBoxDimension($('img.imagebox-main', parent));
         });
         $('.zoomout-command').click(function(){
-            var par = $(this).closest('.crop');
-            $('img.imagebox-main', par).cropper('zoom',-0.1);            
+            var parent = $(this).closest('.crop');
+            $('img.imagebox-main', parent).cropper('zoom',-0.05);
+            var zoom = $(parent).data('zoom') - 0.05;
+            $(parent).data('zoom',zoom);
+            //ImageBox2.setCropBoxDimension($('img.imagebox-main', parent));
         });
+        $(window).resize();
     },
-    setCropDimension : function(img){
-        var cropWidth = $(img).closest('.crop').data('max-width');
-        var cropHeight = $(img).closest('.crop').data('max-height');  
-        img.cropper('setCropBoxData',{width: cropWidth, height: cropHeight, x:0, y:0});
-    },
-    crop : function(obj)
+    initCropBox : function(img)
     {
-        FormController.execute($('img', obj));
+        var options = {
+            viewMode : 0,
+            modal: true,
+            dragMode: 'none',
+            responsive: true,
+            cropBoxResizable : false,
+            data : true,
+            zoomOnWheel: false,
+            crop: function(e) {
+                // Output the result data for cropping image.
+                var zoom = $(this).closest('.crop').data('zoom') + 0;
+                
+                var imgData = $(this).cropper('getImageData');
+                //var factor = imgData.width / imgData.naturalWidth;
+                var crpData = imgData.naturalWidth * zoom + ',';
+                    crpData += imgData.naturalHeight * zoom + ',';
+                    crpData += (e.x * zoom) + ',';
+                    crpData += (e.y * zoom) + ','; 
+                    crpData += (e.width * zoom) + ',';
+                    crpData += (e.height * zoom);
+                $(this).data('action-parameters', crpData);
+            }
+        };
+        $(img).cropper(options).cropper('reset');
+        this.setCropBoxDimension(img);
+    },
+    setCropBoxDimension : function(img)
+    {
+        var cropWidth = $(img).closest('.crop').data('max-width');
+        var cropHeight = $(img).closest('.crop').data('max-height');
+        var imgid = $(img).closest('.crop').attr('id');
+        var imageProperty = $(img).cropper('getImageData');
+        if (imageProperty.naturalWidth > imageProperty.width) {
+            var factor = imageProperty.naturalWidth / imageProperty.width;
+            cropWidth = cropWidth / factor;
+            cropHeight = cropHeight / factor;
+        }
+        $(img).cropper('setCropBoxData',{width: cropWidth, height: cropHeight, x:0, y:0, resizable : false});
+    },
+    crop : function(button)
+    {
+        var image = $('img.imagebox-main', $(button).closest('.crop'));
+        FormController.execute(image);
     }
 }
 
 if (window.FormController) {
-    FormController.register('init','BclImageBox',function() {
-        BclImageBox.init();
+    FormController.register('init','ImageBox2',function() {
+        ImageBox2.init();
     });
 }
