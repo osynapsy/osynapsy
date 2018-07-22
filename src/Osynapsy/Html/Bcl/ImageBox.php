4f157588d8cb9c980c3c285324cc7cc5a470680e
Pietro Celeste <p.celeste@osynapsy.org>
2018-07-22 13:59:06 +0200

    Added data-zoom parameter

--- src/Osynapsy/Html/Bcl/ImageBox.php
+++ src/Osynapsy/Html/Bcl/ImageBox.php
@@ -132,6 +132,7 @@ class ImageBox extends Component
         $this->att('data-max-width', $this->image['maxwidth']);
         $this->att('data-max-height', $this->image['maxheight']);
         $this->att('class','crop',true);
+        $this->att('data-zoom','1');
         $this->toolbar->add('<button type="button" class="crop-command btn btn-info btn-sm"><span class="fa fa-crop"></span></button> ');
         $this->toolbar->add('<button type="button" class="zoomin-command btn btn-info btn-sm"><span class="fa fa-search-plus"></span></button> ');
         $this->toolbar->add('<button type="button" class="zoomout-command btn btn-info btn-sm"><span class="fa fa-search-minus"></span></button> ');
