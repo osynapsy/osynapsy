08de8d74e6ba8ac271426606f6828d87c38f78c3
Pietro Celeste <p.celeste@osynapsy.org>
2019-04-13 18:59:23 +0200

    Correct inerface __construct

--- src/Osynapsy/Mvc/InterfaceController.php
+++ src/Osynapsy/Mvc/InterfaceController.php
@@ -12,11 +12,10 @@
 namespace Osynapsy\Mvc;
 
 use Osynapsy\Http\Request;
-use Osynapsy\Db\DbFactory;
 
 interface InterfaceController
 {
-    public function __construct(Request $request = null, DbFactory $db = null, $appController = null);
+    public function __construct(Request $request = null, $appController = null);
     
     public function getResponse();
 }
