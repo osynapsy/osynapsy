e4199e2010b35cbbe55821c7ca17350b7455f454
Pietro Celeste <p.celeste@osynapsy.org>
2019-05-22 15:15:01 +0200

    improve code

--- src/Osynapsy/Network/Rest.php
+++ src/Osynapsy/Network/Rest.php
@@ -21,13 +21,18 @@ class Rest
             curl_setopt($ch, CURLOPT_PROXY, $proxy[0]);
             curl_setopt($ch, CURLOPT_PROXYPORT, $proxy[1]);
         }
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, \CURLOPT_COOKIEFILE, true);
+        curl_setopt($ch, \CURLOPT_COOKIEJAR, true);
+               curl_setopt($ch, \CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, \CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36');
+        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, "GET");
         //curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
        return curl_exec($ch);  
     }
     
-    public static function post($url, $data, $header = null)
+    public static function post($url, $data, $header = null, array $options = [])
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -35,16 +40,25 @@ class Rest
         if (!empty($header)) {
             curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         }
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, \CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, \CURLOPT_COOKIEFILE, true);
+        curl_setopt($ch, \CURLOPT_COOKIEJAR, true);
+        curl_setopt($ch, \CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36');
         //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
+        foreach($options as $option => $value) {
+            curl_setopt($ch, $option, $value);
+        }
         $resp = curl_exec($ch);
         
         $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+        
+        if ($resp === false) {
+            $resp = curl_errno($ch);
+        }
         curl_close($ch);
         
         return array($contentType, $resp);
