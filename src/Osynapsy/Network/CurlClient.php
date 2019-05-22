98e0aa05e5d05fab9e058ed495c538178e588686
Pietro Celeste <p.celeste@osynapsy.org>
2019-05-22 15:15:13 +0200

    new curl client

---
+++ src/Osynapsy/Network/CurlClient.php
@@ -0,0 +1,70 @@
+<?php
+namespace Osynapsy\Network;
+
+/**
+ * Description of CurlClient
+ *
+ * @author Pietro
+ */
+class CurlRequest
+{
+    const REQUEST_GET = 1;
+    const REQUEST_POST = 2;
+    const REQUEST_USERAGENT = '';
+    
+    private $optionDecoder = [
+        'Proxy' => \CURLOPT_PROXY,
+        'ProxyPort' => \CURLOPT_PROXYPORT,
+        'CookieFile' => \CURLOPT_COOKIEFILE,
+        'SslVerifyHost' => \CURLOPT_SSL_VERIFYHOST,
+        'SslVerifyPeer' => \CURLOPT_SSL_VERIFYPEER,
+        'UserAgent' => \CURLOPT_USERAGENT
+    ];
+    
+    private $parameters = [
+        \CURLOPT_PROXY => null,
+        \CURLOPT_PROXYPORT => null,
+        \CURLOPT_COOKIEFILE => true,
+        \CURLOPT_COOKIEJAR => true,
+        \CURLOPT_SSL_VERIFYHOST => false,
+        \CURLOPT_SSL_VERIFYPEER => false,
+        \CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+        \CURLOPT_RETURNTRANSFER => true        
+    ];
+    private $handle;
+                    
+    public function execute()
+    {
+        foreach ($this->parameters as $option => $value) {
+            curl_setopt($this->handle, $option, $value);
+        }
+        $response = curl_exec($this->handle);
+        return $response;
+    }
+    
+    public function prepare($url, array $data = [], $type = self::REQUEST_GET)
+    {
+        $this->handle = curl_init($url);
+        if ($type === self::REQUEST_GET) {
+            curl_setopt($this->handle, \CURLOPT_CUSTOMREQUEST, "GET");
+        }
+    }
+    
+    public function __call($name, $args)
+    {
+        if (substr($name,0,3) != 'set' || empty($args)) {
+            return false;
+        }
+        $parameter = substr($name, 3);
+        if (!array_key_exists($parameter, $this->optionDecoder)) {
+            return false;
+        }
+        $this->parameters[$this->optionDecoder[$parameter]] = $args[0];
+    }
+    
+    public function setProxy($proxy, $port)
+    {
+        $this->parameters[\CURLOPT_PROXY] = $proxy;
+        $this->parameters[\CURLOPT_PROXYPORT] = $port;
+    }
+}
