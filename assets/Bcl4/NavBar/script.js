7995ee4e5fc34c11ccf7e51e07bbadf8577e58dc
Pietro Celeste <p.celeste@osynapsy.org>
2019-05-22 15:14:38 +0200

    Added css and script for manage submenu

---
+++ assets/Bcl4/NavBar/script.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+Bcl4NavBar = 
+{
+    init : function()
+    {
+        /*$('.osy-bcl4-navbar').on('click','.dropdown-item-submenu',function(){
+            $(this).offset();
+            $(this).next().toggleClass('.d-none').style;
+        });*/
+        $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+            if (!$(this).next().hasClass('show')) {
+                $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+            }
+            var $subMenu = $(this).next(".dropdown-menu");
+            $subMenu.toggleClass('show');
+
+            $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+                $('.dropdown-submenu .show').removeClass("show");
+            });
+            return false;
+        });
+    }
+};
+
+if (window.FormController) {
+    FormController.register('init','osy-bcl4-navbar',function(){
+        Bcl4NavBar.init();
+    });
+}
+
+
