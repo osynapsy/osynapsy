f009551431a8505dd5d03bb8b204467544d8593e
Pietro Celeste <p.celeste@osynapsy.org>
2019-06-01 21:31:06 +0200

    No update

--- assets/js/FormController.js
+++ assets/js/FormController.js
@@ -345,8 +345,8 @@ var Osynapsy = new (function(){
                 win += '    <div class="modal-dialog modal-xs">\n';
                 win += '        <div class="modal-content">\n';
                 win += '            <div class="modal-header">\n';
-                win += '                <button type="button" class="close" data-dismiss="modal">&times;</button>';
-                win += '                <h4 class="modal-title">' + title + '</h4>';
+                win += '                <h4 class="modal-title">' + title + '</h4>';   
+                win += '                <button type="button" class="close" data-dismiss="modal">&times;</button>';               
                 win += '            </div>';
                 win += '            <div class="modal-body" style="padding: 20px">';
                 win += body;
