b3bb514a77610f1003bbe9f59786c749907c6415
Pietro Celeste <p.celeste@osynapsy.org>
2019-06-05 09:28:21 +0200

    Updated name to DataGrid

--- src/Osynapsy/Html/Bcl/DataGrid.php
+++ src/Osynapsy/Html/Bcl/DataGrid.php
@@ -15,117 +15,304 @@ use Osynapsy\Html\Component;
 use Osynapsy\Html\Tag;
 
 class DataGrid extends Component
-{
-    public $data = array();
-    private $columns = array();
-    private $part = array(
-        'head' => null,
-        'body' => null,
-        'foot' => null
-    );
+{      
+    private $columns = [];   
+    private $emptyMessage = 'No data found';
+    private $pagination;
+    private $showHeader = true;
+    private $title;
+    private $rowWidth = 12;
     
     public function __construct($name)
     {
         parent::__construct('div', $name);
-        $this->att('id', $name);
-        $this->part['head'] = new Tag('div');
-        $this->part['body'] = new Tag('div');
-        $this->part['foot'] = new Tag('div');
+        $this->setClass('bcl-datagrid');
+        $this->requireCss('Bcl/DataGrid/style.css');
+        $this->requireJs('Bcl/DataGrid/script.js');
     }
     
+    /**
+     * Internal method to build component      
+     */
     public function __build_extra__()
     {
-        $table = $this->add(
-            new Tag('table')
-        )->att(
-            'class', 'table table-hover'
-        );
-        $table->add(
-            $this->thead(
-                array_keys($this->data[0])
-            )
-        );
-        $tbody = $table->add(
-            new Tag('tbody')
-        );
-        foreach ($this->data as $rec) {
-            $tbody->add(
-                $this->row($rec)
-            );
+        //If datagrid has pager get data from it.
+        if (!empty($this->pagination)) {
+            try {
+                $this->setData($this->pagination->loadData(null, true));
+            } catch (\Exception $e) {
+                $this->printError($e->getMessage());
+            }
+        } 
+        //If Datagrid has title append and show it.
+        if (!empty($this->title)) {
+            $this->add($this->buildTitle($this->title));
         }
+        //If showHeader === true show datagrid columns.
+        if ($this->showHeader) {
+            $this->add($this->buildColumnHead());
+        }
+        //Append Body to datagrid container.
+        $this->add($this->buildBody());
+        //If datagrid has pager append to foot and show it.
+        if (!empty($this->pagination)) {
+            $this->add($this->buildPagination());
+        }        
     }
     
-    private function row($row)
+    private function printError($error)
     {
-        $tr = new Tag('tr');
-        $i = 0;
-        foreach($row as $fieldName => $fieldValue) {
-            $tr->add(
-                $this->cellRow($i, $fieldName, $fieldValue)
-            );
-            $i++;
+        $this->setData([['error' => str_replace(PHP_EOL,'<br>',$error)]]);
+        $this->columns = [];
+        $this->addColumn('Error', 'error', 'col-lg-12');
+    }
+    
+    /**
+     * Internal method for build a Datagrid column head.
+     * 
+     * @return Tag
+     */
+    private function buildColumnHead()
+    {
+        $tr = new Tag('div');
+        $tr->att('class', 'row bcl-datagrid-thead hidden-xs .d-none .d-lg-block .d-xl-none');
+        $orderByFields = $this->pagination ? explode(',', $this->pagination->getOrderBy()) : null;
+        foreach(array_keys($this->columns) as $rawLabel) {
+            $th = $this->columns[$rawLabel]->buildTh($orderByFields);
+            if (empty($th)) {
+                continue;
+            }
+            $tr->add($th);
         }
         return $tr;
     }
     
-    private function cellRow($pos, $fieldName, $fieldValue)
+    /**
+     * Internal metod for build empty message.
+     * 
+     * @param type $body
+     * @return type
+     */
+    private function buildEmptyMessage($body)
     {
-        $td = new Tag('td');
-        if (array_key_exists($pos, $this->columns)) {
-            $function = $this->columns[$pos]['fncCellRow'];
-            $function($fieldValue, $td);
-        }
-        $td->add($fieldValue);
-        return $td;
+        $body->add(
+            '<div class="row"><div class="col-lg-12 text-center">'.$this->emptyMessage.'</div></div>'
+        );
+        return $body;
     }
     
-    private function cellHead($idx, $value) 
-    {    
-        $th = new Tag('th');
-        if (!empty($this->columns[$idx]) && !empty($this->columns[$idx]['fncCellHead'])) {
-            $function = $this->columns[$idx]['fncCellHead'];
-            $function($value, $th);
+    /**
+     * Internal method for build Datagrid body.
+     * 
+     * @return Tag
+     */
+    private function buildBody()
+    {
+        $body = new Tag('div');
+        $body->att('class','bcl-datagrid-body');        
+        if (empty($this->data)) {
+            return $this->buildEmptyMessage($body);
         }        
-        $th->add($value);
-        return $th;
-    }
-    
-    private function thead($rec)
-    {
-        $thead = new Tag('thead');
-        $tr = $thead->add(new Tag('tr'));
-        foreach ($rec as $idx => $columnTitle) {
-            $columnTitle = $this->addColumn($idx, $columnTitle);
-            $thead->add($this->cellHead($idx, $columnTitle));
+        if ($this->rowWidth === 12) {
+            foreach ($this->data as $recIdx => $rec) {            
+                $body->add($this->buildBodyRow($rec));            
+            }
+            return $body;
         }
-        return $thead;
+        $rowClass =  'bcl-datagrid-body-row row col-lg-'.$this->rowWidth;        
+        foreach ($this->data as $recIdx => $rec) {            
+            if (($recIdx) % (12 / $this->rowWidth) === 0) {
+                $row = $body->add(new Tag('div', null, 'row'));
+            }
+            $row->add($this->buildBodyRow($rec, $rowClass));
+        }        
+        return $body;
     }
     
-    public function setColumn($idx, $funcCellRow, $funcCellHead = null)    
+    /**
+     * Internal method for build a Datagrid row
+     * 
+     * @param type $row
+     * @return Tag
+     */
+    private function buildBodyRow($record, $class = 'row bcl-datagrid-body-row')
     {
-        $this->columns[$idx] = [
-            'fncCellRow' => $funcCellRow,
-            'fncCellHead' => $funcCellHead
-        ];        
-    }
-    
-    private function addColumn($idx, $columnTitle)
-    {
-        switch ($columnTitle[0]) {
-            case '_':
-                return null;
-                break;
-            case '$':
-                $columnTitle = substr($columnTitle,1);
-                $this->setColumn($idx, function(&$val, &$cel) {
-                    $cel->att('class','text-right');
-                    $val = number_format($val, 2, ',', '.');
-                });
-            break;
-            default :
-                
-                break;
+        $tr = new Tag('div', null, $class);        
+        foreach ($this->columns as $column) {
+            $tr->add($column->buildTd($tr, $record));
         }
-        return $columnTitle;
+        if (!empty($record['_url_detail'])) {
+            $tr->att('data-url-detail', $record['_url_detail']);
+        }
+        return $tr;
+    }
+    
+    /**
+     * Build Datagrid pagination
+     *      
+     * @return Tag
+     */
+    private function buildPagination()
+    {        
+        $row = new Tag('div', null, 'row bcl-datagrid-pagination');
+        if (empty($this->pagination)) {
+            return $row;
+        }
+        $row->add(new Tag('div', null, 'col-lg-2'))            
+            ->add($this->pagination->getPageDimensionsCombo());
+        $row->add(new Tag('div', null, 'col-lg-4 col-lg-offset-2 offset-lg-2 text-center'))
+             ->add('<label class="" style="margin-top: 30px;">'.$this->pagination->getInfo().'</label>');
+        //if ($this->pagination->getStatistic('pageTotal') > 1) {
+            $row->add(new Tag('div', null, 'col-lg-4 text-right'))
+                ->add($this->pagination)
+                ->setClass('mt-4')->setPosition('end');
+        //}
+        return $row;
+    }
+            
+    /**
+     * Build Datagrid title
+     *      
+     * @return Tag
+     */
+    private function buildTitle()
+    {        
+        $tr = new Tag('div', null, 'row bcl-datagrid-title');
+        $tr->add(new Tag('div', null, 'col-lg-12'))           
+           ->add($this->title);
+        return $tr;
+    }
+               
+    /**
+     * Add a data column view
+     * 
+     * @param type $label of column (show)
+     * @param type $field name of array data field to show
+     * @param type $class css to apply column
+     * @param type $type type of data (necessary for formatting value)
+     * @param callable $function for manipulate data value
+     * @return $this
+     */
+    public function addColumn(
+        $label, 
+        $field,
+        $class = '',
+        $type = 'string',
+        callable $function = null,
+        $fieldOrderBy = null
+    ){
+        if (is_callable($field)) {
+            $function = $field;
+            $field = '';
+        }
+        $column = new DataGridColumn(
+            $label,
+            $field,
+            $class,
+            $type,
+            $function,
+            $fieldOrderBy
+        );
+        $column->setParent($this->id);
+        $this->columns[$label] = $column;
+        return $column;
+        //$this->addColumn($label, $field, $class, $type, $function, $fieldOrderBy);
+    }
+        
+    /**
+     * return pager object
+     * 
+     * @return Pagination object
+     */
+    public function getPagination()
+    {
+        return $this->pagination;        
+    }
+    
+    /**
+     * Hide Header
+     * 
+     * @return $this;
+     */
+    public function hideHeader()
+    {
+        $this->showHeader = false;
+        return $this;
+    }
+    
+    /**
+     * Set array of columns rule
+     * 
+     * @param type $columns
+     * @return $this
+     */
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+        return $this;
+    }
+    
+    /**
+     * Set message to show when no data found.
+     * 
+     * @param type $message
+     * @return $this
+     */  
+    public function setEmptyMessage($message)
+    {
+        $this->emptyMessage = $message;
+        return $this;
+    }
+    
+    /**
+     * Set width of row in bootstrap unit grid (max width = 12)
+     * 
+     * @param int $width
+     */
+    public function setRowWidth($width)
+    {
+        $this->rowWidth = $width;
+        return $this;
+    }
+    
+    /**
+     * Set a pagination object
+     *      * 
+     * @param type $db Handler db connection
+     * @param string $sqlQuery Sql query
+     * @param array $sqlParameters Parameters of sql query
+     * @param integer $pageDimension Page dimension (in row)
+     */
+    public function setPagination($db, $sqlQuery, $sqlParameters, $pageDimension = 10)
+    {
+        $this->pagination = new Pagination(
+            $this->id.(strpos($this->id, '_') ? '_pagination' : 'Pagination'),
+            empty($pageDimension) ? 10 : $pageDimension
+        );
+        $this->pagination->setSql($db, $sqlQuery, $sqlParameters);
+        $this->pagination->setParentComponent($this->id);        
+        return $this->pagination;
+    }
+    
+    /**
+     * Method for set table and rows borders visible
+     * 
+     * return void;
+     */
+    public function setBorderOn()
+    {
+        $this->setClass('bcl-datagrid-border-on');
+    }
+    
+    /**
+     * Set title to show on top of datagrid
+     * 
+     * @param type $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
     }
 }
