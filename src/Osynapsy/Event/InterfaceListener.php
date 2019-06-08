205a4813583efa0f31ce075b349f32c74d958e69
Pietro Celeste <p.celeste@osynapsy.org>
2019-06-08 14:01:55 +0200

    Improve code and bugfix

--- src/Osynapsy/Event/InterfaceListener.php
+++ src/Osynapsy/Event/InterfaceListener.php
@@ -14,13 +14,13 @@ namespace Osynapsy\Event;
 use Osynapsy\Mvc\Controller;
 
 /**
- * Description of Listener
+ * Public method of listener
  *
  * @author pietr
  */
 interface InterfaceListener 
 {
-    public function __construct($controller);
+    public function __construct(Controller $controller);
     
-    public function trigger();
+    public function trigger(Event $event);
 }
