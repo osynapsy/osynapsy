7babe5f2815ae76b52660632372d685fdafdf8af
Pietro Celeste <p.celeste@osynapsy.org>
2019-09-30 22:36:50 +0200

    Added new component autocomplete in the package Bcl4

---
+++ src/Osynapsy/Html/Bcl4/Autocomplete.php
@@ -0,0 +1,26 @@
+<?php
+namespace Osynapsy\Html\Bcl4;
+
+use Osynapsy\Html\Bcl\Autocomplete as AutocompleteBcl3;
+use Osynapsy\Html\Bcl4\InputGroup;
+
+/**
+ * Description of AutoComplete
+ *
+ * @author Pietro
+ */
+class AutoComplete extends AutocompleteBcl3
+{
+    protected $ico = '<span class="fa fa-search input-group-text"></span>';
+    
+    public function __construct($id, $db = null)
+    {
+        parent::__construct($id, $db);
+    }
+    
+    protected function buildAutocomplete()
+    {
+        $autocomplete = new InputGroup($this->id, '', $this->ico);
+        return $autocomplete->setClass('osy-autocomplete');
+    }
+}
