5cdf11ea9dac9e8d2b1e9f890ed5c0af30f45995
Pietro Celeste <p.celeste@osynapsy.org>
2019-10-20 18:08:19 +0200

    Added new component PasswordBox for simulate input of type password

---
+++ src/Osynapsy/Html/Bcl4/PasswordBox.php
@@ -0,0 +1,19 @@
+<?php
+namespace Osynapsy\Html\Bcl4;
+
+use Osynapsy\Html\Bcl\TextBox;
+
+/**
+ * Description of Password
+ *
+ * @author Pietro
+ */
+class PasswordBox extends TextBox
+{        
+    public function __construct($name)
+    {
+        parent::__construct($name);
+        $this->setClass('text-security');
+        $this->requireCss('Bcl4/PasswordBox/text-security.css');
+    }    
+}
