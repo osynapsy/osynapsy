18db907267be4c6061472ae2516c08f15c05a9c2
Pietro Celeste <p.celeste@osynapsy.org>
2019-10-21 12:30:09 +0200

    Added new component form in bcl4

---
+++ src/Osynapsy/Html/Bcl4/Form.php
@@ -0,0 +1,20 @@
+<?php
+namespace Osynapsy\Html\Bcl4;
+
+use Osynapsy\Html\Bcl\Form as Bcl3_Form;
+
+/**
+ * Description of Form
+ *
+ * @author Pietro
+ */
+class Form extends Bcl3_Form
+{
+    public function __construct($name, $mainComponent = 'Panel', $tag = 'form')
+    {
+        parent::__construct($name, $mainComponent, $tag);
+        if ($mainComponent === 'Panel') {
+            $this->body->setClasses('card', 'card-header', 'card-body', 'card-footer');
+        }
+    }
+}
