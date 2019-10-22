afac90e48b43919eab04d33cc1984ee4297a1e0c
Pietro Celeste <p.celeste@osynapsy.org>
2019-10-22 15:55:20 +0200

    Improve code

--- src/Osynapsy/Helper/Net/Curl/Rest.php
+++ src/Osynapsy/Helper/Net/Curl/Rest.php
@@ -13,54 +13,71 @@ namespace Osynapsy\Helper\Net\Curl;
 
 class Rest
 {
-    public static function get($url, $proxy = null)
+    private static $baseurl = '';
+    
+    private static function init($url, $rawheaders, $proxy = null)
     {
-        $ch = curl_init($url);
+        $ch = curl_init(self::$baseurl.$url);
         if (!empty($proxy)) {
             $proxy = explode(':', $proxy);
-            curl_setopt($ch, CURLOPT_PROXY, $proxy[0]);
-            curl_setopt($ch, CURLOPT_PROXYPORT, $proxy[1]);
+            curl_setopt($ch, \CURLOPT_PROXY, $proxy[0]);
+            curl_setopt($ch, \CURLOPT_PROXYPORT, $proxy[1]);
         }
+        self::appendHeaders($ch, $rawheaders);
         curl_setopt($ch, \CURLOPT_COOKIEFILE, true);
         curl_setopt($ch, \CURLOPT_COOKIEJAR, true);
-               curl_setopt($ch, \CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, \CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36');
-        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, "GET");
-        //curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-       return curl_exec($ch);  
-    }
-    
-    public static function post($url, $data, $header = null, array $options = [])
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);        
-        if (!empty($header)) {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        }
-        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, \CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, \CURLOPT_COOKIEFILE, true);
-        curl_setopt($ch, \CURLOPT_COOKIEJAR, true);
         curl_setopt($ch, \CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36');
-        //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        return $ch;
+    }
+    
+    private function appendHeaders($channel, $rawheaders)
+    {
+        if (empty($rawheaders)) {
+            return;
+        }
+        $headers = [];
+        foreach($rawheaders as $key => $value) {
+            $headers[] = strtolower($key).': '.$value; 
+        }
+        curl_setopt($channel, \CURLOPT_HTTPHEADER, $headers);
+    }
+    
+    public static function get($url, $data = [], $headers = [], $proxy = null)
+    {        
+        if (!empty($data)) {
+            $url .= '?'.http_build_query($data);
+        }
+        $ch = self::init($url, $headers);
+        curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, "GET");        
+        return self::getResponse($ch);  
+    }
+    
+    private static function getResponse($channel)
+    {
+        $resp = curl_exec($channel);
+        
+        $contentType = curl_getinfo($channel, CURLINFO_CONTENT_TYPE);
+        $httpCode    = curl_getinfo($channel, CURLINFO_HTTP_CODE);
+        if ($resp === false) {
+            $resp = curl_errno($channel);
+        }
+        curl_close($channel);
+        
+        return ['http-code' => $httpCode, 'content-type' => $contentType, 'response' => $resp];
+    }
+        
+    public static function post($url, $data, $header = null, array $options = [])
+    {
+        $ch = self::init($url, $header);        
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         foreach($options as $option => $value) {
             curl_setopt($ch, $option, $value);
-        }
-        $resp = curl_exec($ch);
-        
-        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-        $httpCode    = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($resp === false) {
-            $resp = curl_errno($ch);
-        }
-        curl_close($ch);
-        
-        return ['http-code' => $httpCode, 'content-type' => $contentType, 'response' => $resp];
+        }                               
+        return self::getResponse($ch);
     }
     
     public static function postJson($url, $data)
@@ -73,5 +90,10 @@ class Rest
         );
         
         return self::post($url, $json, $header);
+    }
+    
+    public static function setBaseUrl($url)
+    {
+        self::$baseurl = $url;
     }
 }
