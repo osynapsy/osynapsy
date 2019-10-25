5e863b2f77961df12ce33d843257a0ff0df0e4fe
Pietro Celeste <p.celeste@osynapsy.org>
2019-10-25 18:37:22 +0200

    Deleted js with bug

--- assets/Bcl/Tab/script.js
+++ assets/Bcl/Tab/script.js
@@ -1,14 +1,7 @@
 BclTab = {
     init : function()
     {
-        $('.nav-tabs').each(function(){
-           
-           var tabSelected = $('input[type=hidden]',$(this).closest('div')).val();
-           if (tabSelected) {
-               $('a[href="'+tabSelected+'"]',this).tab('show')
-           } else {
-               $('a:first',this).tab('show');
-           }
+        $('.nav-tabs').each(function(){                      
            $('a',this).click(function(){
                var tabid = $(this).attr('href');
                var hdnid = $(this).closest('ul').attr('id').replace('_nav','');
