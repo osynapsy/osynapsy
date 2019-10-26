7d904dc1d3b8fb9a42645e9fd2719e4d1e8480c3
Pietro Celeste <p.celeste@osynapsy.org>
2019-10-26 10:31:11 +0200

    improve code

--- src/Osynapsy/Html/Bcl/TextBox.php
+++ src/Osynapsy/Html/Bcl/TextBox.php
@@ -15,7 +15,7 @@ use Osynapsy\Html\Ocl\TextBox as OclTextBox;
 
 class TextBox extends OclTextBox
 {
-    public function __construct($name, $class='')
+    public function __construct($name, $class = '')
     {
         parent::__construct($name);
         $this->att('class',trim('form-control '.$class),true);
