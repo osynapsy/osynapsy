c5b84630e631e1bd2b87ff05fcc1cc5c81c64a9d
Pietro Celeste <p.celeste@osynapsy.org>
2019-11-23 22:48:06 +0100

    Improve code

--- src/Osynapsy/Http/ResponseHtmlOcl.php
+++ src/Osynapsy/Http/ResponseHtmlOcl.php
@@ -14,41 +14,59 @@ namespace Osynapsy\Http;
 use Osynapsy\Html\Tag;
 use Osynapsy\Html\Component;
 
+/**
+ * Build a special Html Response fro View with Ocl and Bcl view
+ * 
+ */
 class ResponseHtmlOcl extends ResponseHtml
 {
     protected function buildResponse()
     {
-        $componentIds = array();
+        $componentIds = [];
         if (!empty($_REQUEST['ajax'])) {
             $componentIds = is_array($_REQUEST['ajax']) ? $_REQUEST['ajax'] : array($_REQUEST['ajax']);
         }
         if (!empty($componentIds)) {
-            $this->resetTemplate();
-            $this->resetContent();
-            $response = new Tag('div');
-            $response->att('id','response');
-            foreach($componentIds as $id) {
-                $response->add(Component::getById($id));                    
-            }
-            $this->addContent($response);       
+            $this->buildComponents($componentIds);      
             return;
+        }        
+        $this->processComponentRequirements(Component::getRequire());
+    }
+    
+    private function buildComponents($componentIds)
+    {
+        $this->resetTemplate();
+        $this->resetContent();
+        $response = new Tag('div','response');
+        foreach($componentIds as $id) {
+            $response->add(Component::getById($id));                    
         }
-        if (!$requires = Component::getRequire()) {
+        $this->addContent($response); 
+    }
+    
+    private function processComponentRequirements($requires)
+    {
+        if (empty($requires)) {
             return;
         }
         foreach ($requires as $type => $urls) {
-            foreach ($urls as $url){
-                switch($type) {
-                    case 'js':
-                        $this->addJs($url);
-                        break;
-                    case 'jscode':
-                        $this->addJsCode($url);
-                        break;
-                    case 'css':
-                        $this->addCss($url);
-                        break;
-                }
+            $this->appendRequirement($type, $urls);
+        }
+    }
+    
+    private function appendRequirement($type, $urls)
+    {
+        foreach ($urls as $url){
+            switch($type) {
+                case 'js':
+                    $this->addJs($url);
+                    break;
+                case 'jscode':
+                    $this->addJsCode($url);
+                    break;
+                case 'css':
+                    $this->addCss($url);
+                    break;
             }
         }
     }
