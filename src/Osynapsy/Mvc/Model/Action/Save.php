cc5638643ce13bf2188c3259227fea0a3b1172c4
Pietro Celeste <p.celeste@osynapsy.org>
2020-03-14 23:24:00 +0100

    Improve code

--- src/Osynapsy/Mvc/Model/Action/Save.php
+++ src/Osynapsy/Mvc/Model/Action/Save.php
@@ -11,23 +11,50 @@ use Osynapsy\Mvc\Action\Base;
  */
 class Save extends Base
 {                
+    protected $events = [
+        'beforeExecute' => null,
+        'beforeInsert' => null,    
+        'beforeUpdate' => null,    
+        'beforeUpload' => null,
+        'afterExecute' => null,    
+        'afterInsert'  => null,
+        'afterUpdate'  => null,
+        'afterUpload'  => null
+    ];
+    
+    public function __construct()
+    {
+        $this->setTrigger('afterExecute', [$this, 'afterExecute']);
+        $this->setTrigger('afterInsert', [$this, 'afterInsert']);
+        $this->setTrigger('afterUpdate', [$this, 'afterUpdate']);
+        $this->setTrigger('afterUpload', [$this, 'afterUpload']);
+    }
+    
     public function execute()
     {        
         try {
+            $this->executeTrigger('beforeExecute');
             $this->getController()->getModel()->save();
-            $this->afterExecute();
+            $this->executeTrigger('afterExecute');
         } catch (ModelErrorException $e) {
             $this->sendErrors($e->getErrors());
         } catch (\Exception $e) {
             $this->sendErrors(['alert' => $e->getMessage()]);
         }
     }
-            
-    protected function afterExecute()
+    
+    protected function executeTrigger($eventId)
+    {
+        if (empty($this->events[$eventId])) {
+            return;
+        }        
+        call_user_func($this->events[$eventId], $this);
+    }
+
+    public function afterExecute()
     {
         if ($this->getModel()->uploadOccurred) {
-            $this->historyPushState($this->getModel()->getLastId());
-            $this->pageRefresh();
+            $this->afterUpload();
             return;
         }
         if ($this->getModel()->behavior === 'insert') {
@@ -36,16 +63,22 @@ class Save extends Base
         }
         $this->afterUpdate();
     }       
-    
-    protected function afterInsert()
+
+    public function afterInsert()
     {
         $this->historyPushState($this->getModel()->getLastId());
         $this->pageRefresh();
     }
-    
-    protected function afterUpdate()
+
+    public function afterUpdate()
     {
         $this->pageBack();
+    }
+    
+    public function afterUpload()
+    {
+        $this->historyPushState($this->getModel()->getLastId());
+        $this->pageRefresh();
     }
     
     protected function pageBack()
@@ -71,5 +104,10 @@ class Save extends Base
         foreach($errors as $fieldHtml => $error) {
             $this->getController()->getResponse()->error($fieldHtml, $error);
         }
-    }        
+    }
+
+    public function setTrigger($event, callable $function)
+    {
+        $this->events[$event] = $function;
+    }
 }
