5011cc5bb59d492501fe09d41166bdb371d80ea2
Pietro Celeste <p.celeste@osynapsy.org>
2020-04-28 11:26:36 +0200

    Impelement setMessage e setCode for dynamic change of error message/code

--- src/Osynapsy/Kernel/KernelException.php
+++ src/Osynapsy/Kernel/KernelException.php
@@ -21,9 +21,18 @@ class KernelException extends \Exception
     private $submessage;
     
     public function __construct($message, $code = 0, Exception $previous = null)
-    {
-        // make sure everything is assigned properly
+    {        
         parent::__construct($message, $code, $previous);
+    }
+    
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+    
+    public function setCode($code)
+    {
+        $this->code = $code;
     }
     
     public function setInfoMessage($message)
