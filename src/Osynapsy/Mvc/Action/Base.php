4ed20f1116e42d4bc4d5586f05fbaa09839d9a1c
Pietro Celeste <p.celeste@osynapsy.org>
2020-07-10 08:31:52 +0200

    Added new method raiseException

--- src/Osynapsy/Mvc/Action/Base.php
+++ src/Osynapsy/Mvc/Action/Base.php
@@ -14,51 +14,56 @@ abstract class Base implements InterfaceAction
     protected $controller;
     protected $parameters;
     protected $triggers = [];
-    
+
     abstract public function execute();
-    
+
     protected function executeTrigger($eventId)
     {
         if (empty($this->triggers[$eventId])) {
             return;
-        }        
+        }
         call_user_func($this->triggers[$eventId], $this);
     }
-    
+
     public function getController()
     {
         return $this->controller;
     }
-    
+
     public function getDb() : InterfaceDbo
     {
         return $this->controller->getDb();
     }
-    
+
     public function getModel()
     {
         return $this->getController()->getModel();
     }
-    
+
     public function getResponse()
     {
         return $this->getController()->getResponse();
     }
-    
+
     public function setController(Controller $controller)
     {
         $this->controller = $controller;
     }
-    
+
     public function setParameters(array $parameters)
     {
         $this->parameters = $parameters;
     }
-    
+
     public function setTrigger(array $events, callable $function)
     {
         foreach ($events as $event) {
             $this->triggers[$event] = $function;
         }
+    }
+
+    protected function raiseException($message, $id = 100)
+    {
+        throw new \Exception($message, $id);
     }
 }
