53c8eec6bdc34d258ccb2cc5e0ed686fec9f029f
Pietro Celeste <p.celeste@osynapsy.org>
2020-07-17 16:02:34 +0200

    Added new formatting type

--- src/Osynapsy/Html/Bcl/DataGridColumn.php
+++ src/Osynapsy/Html/Bcl/DataGridColumn.php
@@ -8,16 +8,17 @@ use Osynapsy\Html\Tag;
  *
  * @author pietr
  */
-class DataGridColumn 
-{        
+class DataGridColumn
+{
     const FIELD_TYPE_DATE_EU = 'date';
     const FIELD_TYPE_MONEY = 'money';
     const FIELD_TYPE_EURO  = 'euro';
+    const FIELD_TYPE_INTEGER = 'integer';
     const FIELD_TYPE_DOLLAR  = 'dollar';
     const FIELD_TYPE_CHECKBOX = 'check';
     const FIELD_TYPE_COMMAND = 'commands';
-    
-    private $properties = [        
+
+    private $properties = [
         'dimension' => [
             'xs' => 12,
             'sm' => 12,
@@ -29,35 +30,35 @@ class DataGridColumn
         'function' => null,
         'class' => null,
         'classTd' => [],
-        'label' => '&nbsp;',        
+        'label' => '&nbsp;',
     ];
     private $parentId;
-    
+
     public function __construct(
-        $label, 
+        $label,
         $field,
         $class = '',
         $type = 'string',
         callable $function = null,
         $fieldOrderBy = null
-    ){        
+    ){
         $this->properties['label'] = $label;
         $this->properties['field'] = $field;
         $this->properties['type'] = $type;
-        $this->properties['class'] = $class;                
+        $this->properties['class'] = $class;
         $this->properties['function'] = $function;
         $this->properties['fieldOrderBy'] = empty($fieldOrderBy) ? $field : $fieldOrderBy;
         $this->addClassTd([$class]);
     }
-    
+
     private function builCheckBoxLabel()
     {
         return '<span class="fa fa-check bcl-datagrid-th-check-all" data-field-class="'.$this->parentId.''.$this->properties['field'].'"></span>';
     }
-    
+
     /**
-     * Build a head cell of DataGrid2 component    
-     * 
+     * Build a head cell of DataGrid2 component
+     *
      * @param array $orderedFields
      * @return Tag
      */
@@ -68,23 +69,23 @@ class DataGridColumn
             return;
         } elseif ($rawLabel[0] == '_') {
             return;
-        }        
-        if ($this->properties['type'] === self::FIELD_TYPE_CHECKBOX) {            
+        }
+        if ($this->properties['type'] === self::FIELD_TYPE_CHECKBOX) {
             $rawLabel = $this->builCheckBoxLabel();
         }
         $th = new Tag('div', null, $this->properties['class'].' bcl-datagrid-th');
-        $th->add(new Tag('span'))->add($rawLabel);        
-        $this->buildThOrderByDummy($th, $orderedFields);                
+        $th->add(new Tag('span'))->add($rawLabel);
+        $this->buildThOrderByDummy($th, $orderedFields);
         return $th;
     }
-    
+
     public function buildThOrderByDummy($th, $orderedFields)
     {
         if ($this->properties['type'] === self::FIELD_TYPE_CHECKBOX) {
             return;
         }
         $orderByField = $this->properties['fieldOrderBy'];
-        $th->att('data-idx', $orderByField)->att('class', 'bcl-datagrid-th-order-by', true);        
+        $th->att('data-idx', $orderByField)->att('class', 'bcl-datagrid-th-order-by', true);
         if (empty($orderedFields)) {
             return;
         }
@@ -96,10 +97,10 @@ class DataGridColumn
             }
         }
     }
-    
+
     /**
-     * Build a body cell of DataGrid2 component    
-     * 
+     * Build a body cell of DataGrid2 component
+     *
      * @param Tag $tr
      * @param type $record
      * @return Tag
@@ -111,37 +112,41 @@ class DataGridColumn
             $properties['function'] = $properties['field'];
             $value = null;
         } elseif (!array_key_exists($properties['field'], $record)) {
-            $value = '<label class="label label-warning">No data found</label>';            
+            $value = '<label class="label label-warning">No data found</label>';
         } else {
-            $value = $record[$properties['field']]; 
+            $value = $record[$properties['field']];
         }
-        $td = new Tag('div', null, 'bcl-datagrid-td');            
+        $td = new Tag('div', null, 'bcl-datagrid-td');
         $td->add($this->valueFormatting($value, $td, $properties, $record, $tr));
         return $td;
     }
-    
+
     /**
      * Format a value of cell for correct visualization
-     * 
+     *
      * @param string $value to format.
      * @param object $cell container of value
-     * @param type $properties 
+     * @param type $properties
      * @param type $rec record which contains value.
      * @param type $tr row container object
      * @return string
      */
     public function valueFormatting($value, &$cell, $properties, $rec, &$tr)
-    {        
+    {
         switch($properties['type']) {
             case self::FIELD_TYPE_CHECKBOX:
                 if (empty($value)) {
                     break;
-                }                       
-                $value = $this->buildCheckBox($value);                
+                }
+                $value = $this->buildCheckBox($value);
                 break;
             case self::FIELD_TYPE_DATE_EU:
-                $datetime = \DateTime::createFromFormat('Y-m-d', $value);                
+                $datetime = \DateTime::createFromFormat('Y-m-d', $value);
                 $value = $datetime === false ? $value : $datetime->format('d/m/Y');
+                $properties['classTd'][] = 'text-center';
+                break;
+            case self::FIELD_TYPE_INTEGER:
+                $properties['classTd'][] = 'text-right';
                 break;
             case self::FIELD_TYPE_EURO:
             case self::FIELD_TYPE_MONEY:
@@ -152,16 +157,16 @@ class DataGridColumn
             case self::FIELD_TYPE_COMMAND:
                 $properties['classTd'][] = 'cmd-row';
                 break;
-        }        
-        if (!empty($properties['function'])) {
-            $value = $properties['function']($value, $cell, $rec, $tr);            
         }
-        if (!empty($properties['classTd'])) {            
+        if (!empty($properties['function'])) {
+            $value = $properties['function']($value, $cell, $rec, $tr);
+        }
+        if (!empty($properties['classTd'])) {
             $cell->att('class', implode(' ', $properties['classTd']), true);
         }
         return ($value != '0' && empty($value)) ? '&nbsp;' : $value;
     }
-    
+
     private function formatCurrencyValue($rawValue, $type)
     {
         $value = '';
@@ -180,10 +185,10 @@ class DataGridColumn
         }
         return $value;
     }
-    
+
     private function buildCheckBox($value)
     {
-        $class = $this->parentId.''.$this->properties['field'];        
+        $class = $this->parentId.''.$this->properties['field'];
         $checkbox = new Tag('input');
         $checkbox->att([
             'type' => 'checkbox',
@@ -196,12 +201,12 @@ class DataGridColumn
         }
         return $checkbox->get();
     }
-    
+
     public function setParent($id)
     {
         $this->parentId = $id;
     }
-    
+
     public function addClassTd(array $class)
     {
         $this->properties['classTd'] = array_merge(
