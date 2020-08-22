70440f8bc59bb5be8ec00928dbf9ea71e8b11728
Pietro Celeste <p.celeste@osynapsy.org>
2020-08-22 08:54:05 +0200

    Extrapulate code for manage history in History.js module

---
+++ assets/js/History.js
@@ -0,0 +1,45 @@
+var Osynapsy = Osynapsy || {};
+
+Osynapsy.History =
+{
+    save : function()
+    {
+        var hst = [];
+        var arr = [];
+        if (sessionStorage.history){
+            hst = JSON.parse(sessionStorage.history);
+        }
+        $('input,select,textarea').not('.history-skip').each(function(){
+            switch ($(this).attr('type')) {
+                case 'submit':
+                case 'button':
+                case 'file':
+                    return true;
+                case 'checkbox':
+                    if (!$(this).is(':checked')) {
+                        return true;
+                    }
+                    break;
+            }
+            if ($(this).attr('name')) {
+                arr.push([$(this).attr('name'), $(this).val()]);
+            }
+        });
+        hst.push({url : window.location.href, parameters : arr});
+        sessionStorage.history = JSON.stringify(hst);
+    },
+    back : function()
+    {
+        if (!sessionStorage.history) {
+            history.back();
+        }
+        var hst = JSON.parse(sessionStorage.history);
+        var stp = hst.pop();
+        if (Osynapsy.isEmpty(stp)) {
+            history.back();
+            return;
+        }
+        sessionStorage.history = JSON.stringify(hst);
+        Osynapsy.post(stp.url, stp.parameters);
+    }
+};
