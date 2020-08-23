619934d351143445129359ba900c2106a67e4e4d
Pietro Celeste <p.celeste@osynapsy.org>
2020-08-23 17:27:24 +0200

    Bugfix

--- assets/Bcl/FileBox/script.js
+++ assets/Bcl/FileBox/script.js
@@ -2,22 +2,20 @@ BclFileBox =
 {
     init : function()
     {
-        $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-            console.log(numFiles);
-            console.log(label);
+        $('.btn-file :file').on('fileselect', function(event, numFiles, label) {            
             $('input[type=text]',$(this).closest('.input-group')).val(label);
         });
         $(document).on('change', '.btn-file :file', function() {
-            var input = $(this),
-                numFiles = input.get(0).files ? input.get(0).files.length : 1,
-                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+            let input = $(this);
+            let numFiles = input.get(0).files ? input.get(0).files.length : 1;
+            let label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
             input.trigger('fileselect', [numFiles, label]);
         });
     }
 }
 
 if (window.Osynapsy){    
-    Osynapsy.register('BclFileBox',function(){
+    Osynapsy.plugin.register('BclFileBox',function(){
         BclFileBox.init();
     });
 }
