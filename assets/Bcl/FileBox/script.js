a90cc47288cb4f82a07e5d5877d7cef4ba768573
Pietro Celeste <p.celeste@osynapsy.org>
2020-08-29 14:38:01 +0200

    Deleted reference with jquery code

--- assets/Bcl/FileBox/script.js
+++ assets/Bcl/FileBox/script.js
@@ -1,18 +1,13 @@
 BclFileBox = 
-{
+{    
     init : function()
-    {
-        $('.btn-file :file').on('fileselect', function(event, numFiles, label) {            
-            $('input[type=text]',$(this).closest('.input-group')).val(label);
-        });
-        $(document).on('change', '.btn-file :file', function() {
-            let input = $(this);
-            let numFiles = input.get(0).files ? input.get(0).files.length : 1;
-            let label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-            input.trigger('fileselect', [numFiles, label]);
+    {                
+        Osynapsy.element('body').on('change', "input[type='file']", function(event) {
+            self = event.target;
+            self.closest('.input-group').querySelector("input[type='text']").value = self.files[0].name;            
         });
     }
-}
+};
 
 if (window.Osynapsy){    
     Osynapsy.plugin.register('BclFileBox',function(){
