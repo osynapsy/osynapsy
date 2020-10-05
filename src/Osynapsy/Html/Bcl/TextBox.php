6d319db5b900d3fd4aea11f08f51aaffcb1f6a6a
Pietro Celeste <p.celeste@osynapsy.org>
2020-10-05 12:11:53 +0200

    Added new method iMask for setting vanillajs mask on textbox

--- src/Osynapsy/Html/Bcl/TextBox.php
+++ src/Osynapsy/Html/Bcl/TextBox.php
@@ -26,6 +26,29 @@ class TextBox extends OclTextBox
     const MASK_TIME = 25;
     const MASK_EMAIL = 60;
     const MASK_IP = 70;
+    const IMASK_INTEGER = 10;
+    const IMASK_NUMBER = 11;
+    const IMASK_CURRENCY = 12;
+    const IMASK_DATE = 20;
+
+    protected $imask = [
+        self::IMASK_INTEGER => [
+            'id' => 'Number',
+            'class' => 'text-right'
+        ],
+        self::IMASK_NUMBER  => [
+            'id' => 'Number',
+            'class' => 'text-right'
+        ],
+        self::IMASK_CURRENCY => [
+            'id' => 'Currency',
+            'class' => 'text-right'
+        ],
+        self::IMASK_DATE => [
+            'id' => 'Date',
+            'class' => 'text-center'
+        ]
+    ];
 
     private $masks = [
         //convert boolean value true = 1 and false = 0
@@ -118,5 +141,17 @@ class TextBox extends OclTextBox
         $this->requireJsCode("$(':input').inputmask();");
         $this->att('data-inputmask', $maskraw);
         return $this;
+    }
+
+    public function setIMask($iMaskId)
+    {
+        if (!array_key_exists($iMaskId, $this->imask)) {
+            throw new \Exception("TextBox {$this->id} : iMask format {$iMaskId} not regnized");
+        }
+        $mask = $this->imask[$iMaskId];
+        $this->requireJs('Lib/imask-6.0.5/imask.js');
+        $this->requireJs('Bcl/InputMask/script.js');
+        $this->setClass(trim('input-mask '.$mask['class']));
+        $this->att('data-imask', $mask['id']);
     }
 }
