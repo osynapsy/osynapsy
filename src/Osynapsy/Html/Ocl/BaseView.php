0e764b0c2dcaabf660f4bc96428f49f4d2f9a4e0
Pietro Celeste <p.celeste@osynapsy.org>
2020-11-07 17:45:09 +0100

    Added new method addMeta for adding metatag at html page

--- src/Osynapsy/Html/Ocl/BaseView.php
+++ src/Osynapsy/Html/Ocl/BaseView.php
@@ -16,14 +16,14 @@ use Osynapsy\Mvc\Controller;
 abstract class BaseView
 {
     protected $components = array();
-    private $controller;       
-    
+    private $controller;
+
     public function __construct(Controller $controller, $title = '')
     {
-        $this->controller = $controller;        
-        $this->setTitle($title);        
+        $this->controller = $controller;
+        $this->setTitle($title);
     }
-    
+
     protected function add($part)
     {
         $this->getController()->getResponse()->send($part);
@@ -31,56 +31,63 @@ abstract class BaseView
             return $part;
         }
     }
-    
+
+    public function addCss($path)
+    {
+        $this->getController()->getResponse()->addCss($path);
+    }
+
+    public function addJs($path)
+    {
+        $this->getController()->getResponse()->addJs($path);
+    }
+
+    public function addJsCode($code)
+    {
+        $this->getController()->getResponse()->addJsCode($code);
+    }
+
+    public function addMeta($property ,$content)
+    {
+        $meta = new \Osynapsy\Html\Tag\Tag('meta');
+        $meta->att(['property' => $property, 'content' => $content]);
+        $this->getController()->getResponse()->addContent($meta, 'meta');
+    }
+
+    public function addStyle($style)
+    {
+        $this->getController()->getResponse()->addStyle($style);
+    }
+
     public function get()
     {
-        return $this->init();            
+        return $this->init();
     }
 
     public function getController()
     {
         return $this->controller;
     }
-    
+
     public function getModel()
     {
         return $this->getController()->getModel();
     }
-    
+
     public function getDb()
     {
         return $this->getController()->getDb();
     }
-    
+
     public function setTitle($title)
     {
         $this->getController()->getResponse()->addContent($title,'title');
     }
-    
-    public function addJs($path)
-    {    
-        $this->getController()->getResponse()->addJs($path);
-    }
-    
-    public function addCss($path)
-    {    
-        $this->getController()->getResponse()->addCss($path);
-    }
-    
-    public function addJsCode($code)
-    {
-        $this->getController()->getResponse()->addJsCode($code);
-    }
-    
-    public function addStyle($style)
-    {
-        $this->getController()->getResponse()->addStyle($style);
-    }
-    
+
     public function __toString()
     {
         return $this->get().'';
     }
-    
+
     abstract public function init();
 }
