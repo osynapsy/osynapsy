5082fae886a571d5d515edbb6bb35fa388cc71fd
Pietro Celeste <p.celeste@osynapsy.org>
2020-11-16 19:50:50 +0100

    bugfix

--- src/Osynapsy/Mvc/Model/Action/CropImage.php
+++ src/Osynapsy/Mvc/Model/Action/CropImage.php
@@ -15,14 +15,14 @@ class CropImage extends Base
     protected $fieldModel;
     protected $fileName;
     protected $imageHandler;
-            
+
     public function execute()
-    {        
+    {
         try {
             $this->loadDocumentRoot();
             $this->loadModelField();
             $this->loadFileName();
-            $this->loadImage();        
+            $this->loadImage();
             $this->cropImage();
             $this->resizeImage();
             $this->saveImageOnDisk();
@@ -31,7 +31,7 @@ class CropImage extends Base
             $this->getResponse()->alertJs($e->getMessage());
         }
     }
-    
+
     protected function loadDocumentRoot()
     {
         $this->documentRoot = filter_input(\INPUT_SERVER, 'DOCUMENT_ROOT');
@@ -39,55 +39,55 @@ class CropImage extends Base
             $this->documentRoot .= '/';
         }
     }
-    
+
     protected function loadFileName()
-    {        
+    {
         $fieldDbName = $this->fieldModel->name;
         $this->fileName = $this->documentRoot.$this->getModel()->getRecord()->get($fieldDbName);
     }
-    
+
     protected function loadModelField()
     {
-        $this->getModel()->find(); 
-        $this->fieldModel = $this->getModel()->getField($this->parameters[4]);
+        $this->getModel()->find();
+        $this->fieldModel = $this->getModel()->getField($this->getParameter(4));
     }
-    
+
     protected function loadImage()
-    {            
+    {
         $this->imageHandler = new Image($this->fileName);
-    }        
-    
-    protected function cropImage()
-    {                                
-        $cropWidth = $this->parameters[0];
-        $cropHeight = $this->parameters[1];
-        $cropX = $this->parameters[2];
-        $cropY = $this->parameters[3];                
-        $this->imageHandler->crop($cropX, $cropY, $cropWidth, $cropHeight);        
     }
-    
+
+    protected function cropImage()
+    {
+        $cropWidth = $this->getParameter(0);
+        $cropHeight = $this->getParameter(1);
+        $cropX = $this->getParameter(2);
+        $cropY = $this->getParameter(3);
+        $this->imageHandler->crop($cropX, $cropY, $cropWidth, $cropHeight);
+    }
+
     protected function resizeImage()
     {
-        if (empty($this->parameters[5]) || empty($this->parameters[6])) {
+        if (empty($this->getParameter(5)) || empty($this->getParameter(6))) {
             return;
         }
-        $resizeWidth = $this->parameters[5];
-        $resizeHeight = $this->parameters[6];
+        $resizeWidth = $this->getParameter(5);
+        $resizeHeight = $this->getParameter(6);
         $this->imageHandler->resizeAdaptive($resizeWidth, $resizeHeight);
     }
-    
+
     protected function refreshPage()
     {
         $this->getResponse()->clearCache();
         $this->getResponse()->go('refresh');
-    }        
-    
+    }
+
     protected function saveFileNameOnDb($fileName)
-    {        
+    {
         $fieldDbName = $this->fieldModel->name;
         $this->getModel()->getRecord()->save([$fieldDbName => $fileName]);
     }
-    
+
     protected function saveImageOnDisk()
     {
         $this->imageHandler->save($this->fileName);
