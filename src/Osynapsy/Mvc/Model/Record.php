1f289ef654593a74a258d5a0585704971b8ac85c
Pietro Celeste <p.celeste@osynapsy.org>
2020-11-18 14:34:18 +0100

    Improve code

--- src/Osynapsy/Mvc/Model/Record.php
+++ src/Osynapsy/Mvc/Model/Record.php
@@ -16,22 +16,22 @@ use Osynapsy\Mvc\Controller;
 use Osynapsy\Mvc\Model\Field;
 
 abstract class Record
-{       
+{
     const BEHAVIOR_INSERT = 'insert';
     const BEHAVIOR_UPDATE = 'update';
     const BEHAVIOR_DELETE = 'delete';
-    
+
     const EVENT_BEFORE_SAVE = 'beforeSave';
     const EVENT_BEFORE_INSERT = 'beforeInsert';
-    const EVENT_BEFORE_UPDATE = 'beforeUpdate';    
+    const EVENT_BEFORE_UPDATE = 'beforeUpdate';
     const EVENT_BEFORE_DELETE = 'beforeDelete';
     const EVENT_AFTER_SAVE = 'afterSave';
     const EVENT_AFTER_INSERT = 'afterInsert';
     const EVENT_AFTER_UPDATE = 'afterUpdate';
     const EVENT_AFTER_DELETE = 'afterDelete';
     const EVENT_AFTER_UPLOAD = 'afterUpload';
-    
-    private $fields = [];    
+
+    private $fields = [];
     private $record;
     private $exception;
     private $controller;
@@ -39,38 +39,38 @@ abstract class Record
     protected $softDelete;
     public $uploadOccurred = false;
     public $behavior;
-    
+
     public function __construct($controller)
     {
-        $this->controller = $controller;        
+        $this->controller = $controller;
         $this->record = $this->record();
         $this->initExternalAction();
         $this->init();
         $this->initRecord();
     }
-    
+
     protected function dispatchEvent($event)
     {
         $this->getController()->getDispatcher()->dispatch(new EventLocal($event, $this));
     }
-    
+
     public function addListenerLocal(callable $trigger, array $eventIDs)
-    {        
+    {
         array_walk($eventIDs, function (&$value, $key, $namespace) { $value = $namespace.'\\'.$value;}, get_class($this));
         $this->getController()->getDispatcher()->addListener($trigger, $eventIDs);
     }
-    
+
     private function initRecord()
     {
         $keys = [];
         foreach($this->fields as $field) {
             if ($field->isPkey()) {
                 $keys[$field->name] = $field->getDefaultValue();
-            }       
-        }        
-            $this->getRecord()->findByAttributes($keys);        
+            }
+        }
+        $this->getRecord()->findByAttributes($keys);
     }
-    
+
     protected function initExternalAction()
     {
         $this->getController()->setExternalAction('save', new \Osynapsy\Mvc\Model\Action\Save());
@@ -79,17 +79,22 @@ abstract class Record
         $this->getController()->setExternalAction('deleteFile', new \Osynapsy\Mvc\Model\Action\DeleteFile());
         $this->getController()->setExternalAction('cropImage', new \Osynapsy\Mvc\Model\Action\CropImage());
     }
-    
+
+    public function getBehavior()
+    {
+        return $this->getRecord()->getBehavior();
+    }
+
     public function getDb()
     {
         return $this->getController()->getDb();
     }
-    
+
     protected function getController() : Controller
     {
         return $this->controller;
     }
-    
+
     public function getException()
     {
         if (empty($this->exception)) {
@@ -97,22 +102,22 @@ abstract class Record
         }
         return $this->exception;
     }
-    
+
     public function getField($fieldId)
     {
         return $this->fields[$fieldId];
     }
-    
+
     public function getLastId()
     {
         return $this->getRecord()->lastAutoincrementId;
     }
-    
-    public function getRecord()
+
+    public function getRecord() : \Osynapsy\Db\Record\InterfaceRecord
     {
         return $this->record;
     }
-    
+
     public function getValidator()
     {
         if (empty($this->validator)) {
@@ -120,21 +125,21 @@ abstract class Record
         }
         return $this->validator;
     }
-    
+
     public function getValue($key)
     {
         return $this->getRecord()->getValue($key);
     }
-    
+
     public function find()
-    {        
+    {
         $values = $this->getRecord()->get();
         $this->loadValuesInRequest($values);
         return $this->getRecord();
     }
-    
+
     public function loadValuesInRequest($values)
-    {        
+    {
         if (empty($values)) {
             return;
         }
@@ -148,31 +153,31 @@ abstract class Record
             }
         }
     }
-    
+
     public function map($fieldNameOnForm, $fieldNameOnRecord = null, $defaultValue = null, $type = 'string')
-    {        
+    {
         $formValue = isset($_REQUEST[$fieldNameOnForm]) ? $_REQUEST[$fieldNameOnForm] : null;
         $field = new Field($this, $fieldNameOnRecord, $fieldNameOnForm, $type, isset($_REQUEST[$fieldNameOnForm]));
-        $field->setValue($formValue, $defaultValue);        
-        $this->fields[$field->html] = $field;        
+        $field->setValue($formValue, $defaultValue);
+        $this->fields[$field->html] = $field;
         return $field;
     }
-    
+
     /**
      * Save values into ActiveRecord
-     * 
+     *
      * @return void
      */
     public function save()
-    {                
+    {
         //Recall before exec method with arbirtary code
-        $this->dispatchEvent(self::EVENT_BEFORE_SAVE);                
+        $this->dispatchEvent(self::EVENT_BEFORE_SAVE);
         //Fill Record with values from html form
         $this->fillRecord();
         //If occurred some error stop db updating and return exception
-        if (!empty($this->exception) && !empty($this->exception->getErrors())) {            
+        if (!empty($this->exception) && !empty($this->exception->getErrors())) {
             throw $this->exception;
-        }        
+        }
         //If where list is empty execute db insert else execute a db update
         if ($this->getRecord()->getBehavior() == 'insert') {
             $this->insert();
@@ -182,11 +187,11 @@ abstract class Record
         //Recall after exec method with arbirtary code
         $this->dispatchEvent(self::EVENT_AFTER_SAVE);
     }
-    
+
     private function fillRecord()
     {
         //skim the field list for check value and build $values, $where and $key list
-        foreach ($this->fields as $field) {            
+        foreach ($this->fields as $field) {
             //Check if value respect rule
             if ($field->existInForm()) {
                 $this->validateField($field);
@@ -201,38 +206,38 @@ abstract class Record
                 continue;
             }
             //Set value in record
-            $this->getRecord()->setValue($field->name, $field->value);            
+            $this->getRecord()->setValue($field->name, $field->value);
         }
     }
-    
+
     private function validateField($field)
-    {        
+    {
         try {
             $this->getValidator()->validate($field);
         } catch(\Exception $e) {
             $this->getException()->setErrorOnField($field, $e->getMessage());
         }
     }
-    
+
     public function insert()
-    {                
-        $this->setBehavior(self::BEHAVIOR_INSERT);        
+    {
+        $this->setBehavior(self::BEHAVIOR_INSERT);
         $this->dispatchEvent(self::EVENT_BEFORE_INSERT);
         $this->getRecord()->save();
         $this->dispatchEvent(self::EVENT_AFTER_INSERT);
     }
 
     public function update()
-    {     
-        $this->setBehavior(self::BEHAVIOR_UPDATE);        
+    {
+        $this->setBehavior(self::BEHAVIOR_UPDATE);
         $this->dispatchEvent(self::EVENT_BEFORE_UPDATE);
-        $this->getRecord()->save();        
+        $this->getRecord()->save();
         $this->dispatchEvent(self::EVENT_AFTER_UPDATE);
     }
-    
+
     public function delete()
-    {    
-        $this->setBehavior(self::BEHAVIOR_DELETE);             
+    {
+        $this->setBehavior(self::BEHAVIOR_DELETE);
         $this->dispatchEvent(self::EVENT_BEFORE_DELETE);
         if (empty($this->softDelete)) {
             $this->getRecord()->delete();
@@ -241,23 +246,23 @@ abstract class Record
         }
         $this->dispatchEvent(self::EVENT_AFTER_DELETE);
     }
-    
+
     public function setBehavior($behavior)
     {
         $this->behavior = $behavior;
     }
-    
+
     public function setValue($fieldId, $value, $defaultValue = null)
     {
         $this->fields[$fieldId]->setValue($value, $defaultValue);
     }
-    
+
     public function softDelete($field, $value)
     {
         $this->softDelete = [$field => $value];
-    }        
-    
-    abstract protected function init();  
-    
-    abstract protected function record();        
+    }
+
+    abstract protected function init();
+
+    abstract protected function record();
 }
