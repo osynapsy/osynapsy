2c6d15233fb51fe99270fb7f8b8c7e52932752f7
Pietro Celeste <p.celeste@osynapsy.org>
2020-12-29 16:46:26 +0100

    Add memory to component

--- src/Osynapsy/Html/Bcl4/Tags.php
+++ src/Osynapsy/Html/Bcl4/Tags.php
@@ -2,6 +2,7 @@
 namespace Osynapsy\Html\Bcl4;
 
 use Osynapsy\Html\Component;
+use Osynapsy\Html\Tag;
 use Osynapsy\Html\Ocl\HiddenBox;
 /**
  * Description of Tags
@@ -11,11 +12,13 @@ use Osynapsy\Html\Ocl\HiddenBox;
 class Tags extends Component
 {
     protected $hiddenId;
+    private $labelClass;
 
-    public function __construct($id)
+    public function __construct($id, $labelClass = 'badge badge-info')
     {
         parent::__construct('div', $id.'Box');
         $this->hiddenId = $id;
+        $this->labelClass = $labelClass;
         $this->setClass('bcl4-tags form-control');
         $this->requireJs('Bcl4/Tags/script.js');
         $this->requireCss('Bcl4/Tags/style.css');
@@ -23,7 +26,29 @@ class Tags extends Component
 
     protected function __build_extra__(): void
     {
-        $this->add(new HiddenBox($this->id));
+        $this->add(new HiddenBox($this->hiddenId));
+        if (!empty($_REQUEST[$this->hiddenId])) {
+            $this->add($this->tagsFactory($_REQUEST[$this->hiddenId]));
+        }
         $this->add('<input type="text" class="bcl4-tags-input" size="1">');
+    }
+
+    protected function tagsFactory($strTags)
+    {
+        $result = new Tag('dummy');
+        $tags = explode('][', $strTags);
+        foreach($tags as $tag) {
+            $result->add($this->tagFactory($tag));
+        }
+        return $result;
+    }
+
+    protected function tagFactory($tag)
+    {
+        $wrapper = new Tag('h5', null, 'd-inline mr-1');
+        $badge = $wrapper->add(new Tag('span', null, $this->labelClass));
+        $badge->add(str_replace(['[',']'], '', $tag));
+        $badge->add(new Tag('span', null, 'fa fa-close bcl4-tags-delete'));
+        return $wrapper;
     }
 }
