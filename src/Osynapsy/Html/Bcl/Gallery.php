38d4a3937aa19c85ff5254f93df1843eb106d54b
Pietro Celeste <p.celeste@osynapsy.org>
2021-04-28 12:11:49 +0200

    Refactoring gallery component

--- src/Osynapsy/Html/Bcl/Gallery.php
+++ src/Osynapsy/Html/Bcl/Gallery.php
@@ -13,85 +13,100 @@ namespace Osynapsy\Html\Bcl;
 
 use Osynapsy\Html\Tag;
 use Osynapsy\Html\Component;
-use Osynapsy\Html\Bcl\ContextMenu;
+use Osynapsy\Html\Bcl\Modal;
+use Osynapsy\Html\Bcl\Button;
+use Osynapsy\Html\Bcl\FileBox;
 
 class Gallery extends Component
 {
-    protected $photos;
-    protected $dim = array(180,180);
-    protected $actions;
+    protected $cellWidth = 4;
+    protected $actions = [];
     protected $defaultPhoto;
     protected $contextMenu;
-    
-    public function __construct($id, $dim = array(180,180))
+    protected $modalViewerDimension;
+
+    public function __construct($id, $cellWidth = 4)
     {
         parent::__construct('div', $id);
-        $this->add(new Tag('span'))
-             ->att('class','gallery');
-        $this->dim = $dim;
+        $this->requireJs('Bcl/FileBox/script.js');
+        $this->add(new Tag('span', null, 'gallery'));
+        $this->cellWidth = $cellWidth;
     }
-    
-    public function addAction($label, $action, $actionField='id')
-    {
-        if (!$this->contextMenu) {
-            $this->contextMenu = new ContextMenu($this->id.'ContextMenu');
-        }
-        $this->contextMenu->addAction($label, $action, $actionField);
-        $this->actions[] = array($label, $action, $actionField);
-    }
-    
-    public function setPhotoList($list)
-    {
-        $this->photos = $list;
-    }
-    
-    public function setDefault($val,$field='id')
-    {
-        $this->defaultPhoto = array($val,$field);
-    }
-    
+
     public function __build_extra__()
     {
-        foreach ($this->photos as $key => $photo) {
-            $div = $this->add(new Tag('div'))->att('class','col-xs-2 col-md-1');
-            $a = $div->add(new Tag('a'))->att('class','thumbnail');
-            if ($this->contextMenu) {
-                $a->att('class','BclContextMenuOrigin',true)
-                  ->att('data-bclcontextmenuid', $this->id.'ContextMenu');
-            }
-            if ($this->defaultPhoto && $this->defaultPhoto[0] == $photo[$this->defaultPhoto[1]]) {
-                $a->att('style','border-color: red;');
-            }
-            $img = $a->add(new Tag('img'))->att('src',$photo['url']);
-            if (!empty($this->dim[0])) {
-                $img->att('style','width: '.$this->dim[0].'px');
-            }
-            if (!empty($this->dim[1])) {
-                $img->att('style','height: '.$this->dim[1].'px',';');
-            }
-            //Create Photo caption
-            $caption = $div->add(new Tag('div'))->att('class','caption text-center');
-            if ($photo['label']) {
-                $caption->add($photo['label']);
-            }
-            //If delete action is set add button delete
-            if (empty($this->actions)) {
-                return;
-            }
-            foreach ($this->actions as $action) {
-                $caption->add(new Tag('button'))
-                        ->att(array(
-                            'class' => 'btn btn-danger cmd-execute',
-                            'type' => 'button',
-                            'data-action' => $action[1],
-                            'data-action-param' => $photo[$action[2]]
-                        ))->add($action[0]);
-                $a->att('data-action-param' , $photo[$action[2]]);
-            }
+        $this->add($this->modalUploadFactory());
+        $this->add($this->modalViewerFactory());
+        $row = $this->add(new Tag('div', null, 'row mb-2'));
+        foreach ($this->data as $photo) {
+            $row->add($this->cellFactory($photo));
         }
-        
-        if ($this->contextMenu) {
-            $this->add($this->contextMenu);
-        }
+        //$row->add($this->cellAddFactory());
+        $this->add($this->buttonOpenModalFactory());
+    }
+
+    protected function cellFactory($photo)
+    {
+        $div = new Tag('div', null, sprintf('text-center col-%s', $this->cellWidth));
+        $div->add($this->thumbnailFactory($photo));
+        return $div;
+    }
+
+    protected function cellAddFactory()
+    {
+        $div = new Tag('div', null, sprintf('text-center col-%s mt-2', $this->cellWidth));
+        $div->add(new Tag('div', null, 'img-thumbnail'))->add(new Tag('span', null, 'fa fa-plus'));
+        return $div;
+    }
+
+    protected function thumbnailFactory($photo)
+    {
+        $img = new Tag('img', null, 'img-thumbnail mt-2');
+        $img->att(['data-toggle' => 'modal', 'data-target' => sprintf('#%sModalViewer', $this->id)]);
+        $img->att('onclick', sprintf("let src = $(this).attr('src'); $('#%sViewer').attr('src', src); $('#%sDeleteImage').attr('data-action-parameters', src);", $this->id, $this->id));
+        $img->att('src', $photo['url']);
+        return $img;
+    }
+
+    protected function buttonOpenModalFactory()
+    {
+        $Button = new Button($this->id.'Add', 'button', 'btn btn-danger btn-block', 'Aggiungi foto');
+        $Button->att(['data-toggle' => 'modal', 'data-target' => '#'.$this->id.'ModalUpload']);
+        return $Button;
+    }
+
+    protected function modalUploadFactory()
+    {
+        $Modal = new Modal($this->id.'ModalUpload', 'Aggiungi foto');
+        $Modal->getPanelBody()->addColumn()->push('Seleziona l\'immagine da aggiungere alla gallery', $this->fileUploadFactory());
+        $Modal->getPanelFoot()->add($this->buttonSendPhotoToGalleryFactory());
+        return $Modal;
+    }
+
+    protected function fileUploadFactory()
+    {
+        return new FileBox($this->id.'File');
+    }
+
+    protected function buttonSendPhotoToGalleryFactory()
+    {
+        $Button = new Button('btnSendPotoTo'.$this->id, 'button', 'btn btn-primary', 'Invia foto');
+        $Button->setAction('addPhotoToGallery', $this->id.'File');
+        return $Button;
+    }
+
+    protected function modalViewerFactory()
+    {
+        $Modal = new Modal($this->id.'ModalViewer', 'Foto', $this->modalViewerDimension);
+        $Modal->getPanelBody()->addColumn()->setClass('text-center')->add(new Tag('img', $this->id.'Viewer', 'img-thumbnail'));
+        $Modal->getPanelFoot()->add($this->buttonDeletePhotoFactory());
+        return $Modal;
+    }
+
+    protected function buttonDeletePhotoFactory()
+    {
+        $Button = new Button($this->id.'DeleteImage', 'button', 'btn btn-danger', 'Elimina foto');
+        $Button->setAction('deletePhotoFromGallery', null, 'click-execute', 'Sei sicuro di voler eliminare la foto corrente (L\'operazione non è reversibile)? ');
+        return $Button;
     }
 }
