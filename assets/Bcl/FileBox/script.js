e5df8a8c0d18bf2bab0d91ce560b95e929e0e6ee
Pietro Celeste <p.celeste@osynapsy.org>
2021-04-28 09:14:35 +0200

    .

--- assets/Bcl/FileBox/script.js
+++ assets/Bcl/FileBox/script.js
@@ -1,15 +1,15 @@
-BclFileBox = 
-{    
+BclFileBox =
+{
     init : function()
-    {                
+    {
         Osynapsy.element('body').on('change', ".btn-file input[type='file']", function(event) {
             self = event.target;
-            self.closest('.input-group').querySelector("input[type='text']").value = self.files[0].name;            
+            self.closest('.input-group').querySelector("input[type='text']").value = self.files[0].name;
         });
     }
 };
 
-if (window.Osynapsy){    
+if (window.Osynapsy){
     Osynapsy.plugin.register('BclFileBox',function(){
         BclFileBox.init();
     });
