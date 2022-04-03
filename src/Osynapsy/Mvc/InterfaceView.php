87e6954e674422555d70cf9a6b06fc2c6c6153b3
Pietro Celeste <p.celeste@osynapsy.net>
2022-04-03 15:14:27 +0200

    Added new interface for view

---
+++ src/Osynapsy/Mvc/InterfaceView.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Osynapsy\Mvc;
+
+/**
+ * Description of InterfaceView
+ *
+ * @author pietr
+ */
+interface InterfaceView
+{
+    public function init();
+    
+    public function get();
+}
