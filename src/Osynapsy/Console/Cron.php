a5e560bc3eb61a2ee70c33eee1b0d7119b9c0aa4
Pietro Celeste <p.celeste@osynapsy.net>
2022-04-10 18:04:42 +0200

    Improve code

--- src/Osynapsy/Console/Cron.php
+++ src/Osynapsy/Console/Cron.php
@@ -20,64 +20,60 @@ use Osynapsy\Kernel;
  *
  * @author Peter
  */
-class Cron 
+class Cron
 {
     private $argv;
     private $script;
     private $kernel;
-    
+
     public function __construct(array $argv)
     {
         $this->script = array_shift($argv);
         $this->argv = $argv;
     }
-    
-    private function load($configuration)
+
+    public function run()
+    {
+        $appConfiguration = $this->loadAppConfiguration($this->argv[0]);
+        $cronJobs = $this->getCronJobs($appConfiguration);
+        if (!empty($cronJobs)) {
+            $this->exec($cronJobs);
+        }
+    }
+
+    private function loadAppConfiguration($instancePath)
+    {
+        if (!is_dir($instancePath)) {
+            return;
+        }
+        $loader = new Loader($instancePath);
+        return $loader->search('app');
+    }
+
+    private function getCronJobs($configuration)
     {
         if (empty($configuration) || !is_array($configuration)) {
             return;
         }
         $jobs = [];
         foreach($configuration as $app => $config) {
-            if (empty($config['cron'])) {
-                continue;
+            if (!empty($config['cron'])) {
+                $jobs[$app] = $config['cron'];
             }
-            $jobs[$app] = $config['cron'];
         }
         return $jobs;
     }
-    
-    private function loadConfiguration()
-    {
-        if (!is_dir($this->argv[0])) {
-            return;
-        }
-        $loader = new Loader($this->argv[0]);
-        return $loader->search('app');
-    }
-    
-    public function run()
-    {
-        $this->exec(
-            $this->load(
-                $this->loadConfiguration()
-            )
-        );
-    }
-    
+
     private function exec($jobs)
     {
-        if (empty($jobs)) {
-            return;
-        }
         $this->kernel = new Kernel($this->argv[0]);
-        foreach($jobs as $appId => $appJobs) {            
+        foreach($jobs as $appId => $appJobs) {
             foreach($appJobs as $jobId => $jobController){
                 $this->execJob($jobId , $appId, $jobController);
             }
         }
     }
-    
+
     private function execJob($jobId, $application, $controller)
     {
         $job = new Route($jobId, null, $application, $controller);
