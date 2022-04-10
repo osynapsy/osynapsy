65f1f398a71174cdba542b23bfc1f5d4295a383b
Pietro Celeste <p.celeste@osynapsy.net>
2022-04-10 11:08:11 +0200

    Improve InterfaceApplication

--- src/Osynapsy/Mvc/InterfaceApplication.php
+++ src/Osynapsy/Mvc/InterfaceApplication.php
@@ -13,10 +13,26 @@ namespace Osynapsy\Mvc;
 
 use Osynapsy\Kernel\Route;
 use Osynapsy\Http\Request;
+use Osynapsy\Http\Response\Base as Response;
+use Osynapsy\Db\Driver\InterfaceDbo;
 
 interface InterfaceApplication
 {
     public function __construct(Route &$route, Request &$request);
 
+    public function getDb(int $key = 0) : InterfaceDbo;
+
+    public function getDbFactory() : \Osynapsy\Db\DbFactory;
+
+    public function getRequest($key = null);
+
+    public function getResponse() : Response;
+
+    public function getRoute() : Route;
+
     public function run();
+
+    public function runAction() : string;
+
+    public function setResponse(Response $response);
 }
