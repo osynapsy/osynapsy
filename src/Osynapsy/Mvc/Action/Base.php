79a7940e5d6d927a9b77034e57a9debf38adc13e
Pietro Celeste <p.celeste@osynapsy.net>
2022-04-10 16:21:43 +0200

    update doc

--- src/Osynapsy/Mvc/Action/Base.php
+++ src/Osynapsy/Mvc/Action/Base.php
@@ -5,9 +5,11 @@ use Osynapsy\Mvc\Controller;
 use Osynapsy\Db\Driver\InterfaceDbo;
 
 /**
- * Description of Base
+ * Base class for implement an external action.
+ * External action is a class which implement all code to respond
+ * frontend action event.
  *
- * @author Pietro
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
 abstract class Base implements InterfaceAction
 {
@@ -15,6 +17,9 @@ abstract class Base implements InterfaceAction
     private $parameters = [];
     protected $triggers = [];
 
+    /**
+     * Main method which controller recall when Frontend action is recalled.
+     */
     abstract public function execute();
 
     protected function executeTrigger($eventId)
@@ -25,41 +30,82 @@ abstract class Base implements InterfaceAction
         call_user_func($this->triggers[$eventId], $this);
     }
 
+    /**
+     * Wrapper of getApp controller method
+     *
+     * @return InstanceApplication
+     */
     public final function getApp()
     {
         return $this->controller->getApp();
     }
 
+    /**
+     * Get current controller instance.
+     *
+     * @return Controller
+     */
     public final function getController()
     {
         return $this->controller;
     }
 
+    /**
+     * Get current database connection
+     *
+     * @return InterfaceDbo
+     */
     public function getDb() : InterfaceDbo
     {
         return $this->controller->getDb();
     }
 
+    /**
+     * Get the current model
+     *
+     * @return Model
+     */
     public function getModel()
     {
         return $this->getController()->getModel();
     }
 
+    /**
+     * Get the n paramenter from the frontedn request
+     *
+     * @param int $index
+     * @return mixed
+     */
     public final function getParameter($index)
     {
         return array_key_exists($index, $this->parameters) ? $this->parameters[$index] : null;
     }
 
+    /**
+     * Get the current response
+     *
+     * @return Response
+     */
     public function getResponse()
     {
         return $this->getController()->getResponse();
     }
 
+    /**
+     * Set controller
+     *
+     * @param Controller $controller
+     */
     public function setController(Controller $controller)
     {
         $this->controller = $controller;
     }
 
+    /**
+     * Set action parameters from frontend
+     *
+     * @param array $parameters
+     */
     public function setParameters(array $parameters)
     {
         $this->parameters = $parameters;
@@ -72,6 +118,13 @@ abstract class Base implements InterfaceAction
         }
     }
 
+    /**
+     * Raise an exception
+     *
+     * @param string $message Exception message
+     * @param int $id
+     * @throws \Exception
+     */
     protected function raiseException($message, $id = 100)
     {
         throw new \Exception($message, $id);
