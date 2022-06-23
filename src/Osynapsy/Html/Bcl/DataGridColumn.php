69645169dc532bc7076f538d25812eb22fe0a33a
Pietro Celeste <p.celeste@osynapsy.net>
2022-06-23 09:24:08 +0200

    Added magic method __get

--- src/Osynapsy/Html/Bcl/DataGridColumn.php
+++ src/Osynapsy/Html/Bcl/DataGridColumn.php
@@ -215,6 +215,11 @@ class DataGridColumn
         $this->properties['classTd'] = [$class];
     }
 
+    public function __get($name)
+    {
+        return $this->properties[$name];
+    }
+    
     public function __set($name, $value)
     {
         $this->properties[$name] = $value;
