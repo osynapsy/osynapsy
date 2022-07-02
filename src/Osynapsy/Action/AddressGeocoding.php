013eda65874ac6dc971cce2c4b6f8c1e11d1da18
Pietro Celeste <p.celeste@osynapsy.net>
2022-07-02 13:44:19 +0200

    Added legal disclaimer

--- src/Osynapsy/Action/AddressGeocoding.php
+++ src/Osynapsy/Action/AddressGeocoding.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Action;
 
 /**
@@ -6,13 +16,13 @@ namespace Osynapsy\Action;
  *
  * @author p.celeste@osynapsy.net
  */
-class AddressGeocoding 
+class AddressGeocoding
 {
     //put your code here
     public static function getLatLng($address)
-    {           
+    {
            $geourl = sprintf("http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false&region=it", trim($address));
-           
+
            // Create cUrl object to grab XML content using $geourl
            $c = curl_init();
            curl_setopt($c, CURLOPT_URL, utf8_encode($geourl));
@@ -23,10 +33,10 @@ class AddressGeocoding
            $resp = trim(curl_exec($c));
            //$r = curl_getinfo($c);
            curl_close($c);
-           // Create SimpleXML object from XML Content           
-           $obj = json_decode($resp);           
+           // Create SimpleXML object from XML Content
+           $obj = json_decode($resp);
            // Print out all of the XML Object
-           if ($obj->status && $obj->status == 'OK') {               
+           if ($obj->status && $obj->status == 'OK') {
                return array(
                    $obj->results[0]->geometry->location->lat,
                    $obj->results[0]->geometry->location->lng
