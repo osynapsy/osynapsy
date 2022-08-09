97d2c52886e999eaa7bd1195f2b655ea99147c1e
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-09 16:28:47 +0200

    Improve code

--- tests/src/Html/TagTest.php
+++ tests/src/Html/TagTest.php
@@ -9,25 +9,25 @@ final class TagTest extends TestCase
     public function testTag(): void
     {
         $Tag = new Tag('span');
-        $this->assertEquals(trim($Tag->get()), '<span></span>');
+        $this->assertEquals((string) $Tag, '<span></span>');
     }
 
     public function testId()
     {
         $Tag = new Tag('span', 'xyz');
-        $this->assertEquals(trim($Tag->get()), '<span id="xyz"></span>');
+        $this->assertEquals((string) $Tag, '<span id="xyz"></span>');
     }
 
     public function testClass()
     {
         $Tag = new Tag('span', null, 'abc');
-	$this->assertEquals(trim($Tag->get()), '<span class="abc"></span>');
+        $this->assertEquals((string) $Tag, '<span class="abc"></span>');
     }
 
     public function testIdClass()
     {
         $Tag = new Tag('span', 'xyz', 'abc');
-        $this->assertEquals(trim($Tag->get()), '<span id="xyz" class="abc"></span>');
+        $this->assertEquals((string) $Tag, '<span id="xyz" class="abc"></span>');
     }
 
     public function testParent()
