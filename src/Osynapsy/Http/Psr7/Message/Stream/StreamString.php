fc1a7630d701398f7ae1078fe03f5475882d5d92
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-14 15:25:06 +0200

    Implements new stream from string

--- src/Osynapsy/Http/Psr7/Message/Stream/StreamString.php
+++ src/Osynapsy/Http/Psr7/Message/Stream/StreamString.php
@@ -6,80 +6,12 @@ namespace Osynapsy\Http\Psr7\Message\Stream;
  *
  * @author Pietro Celeste <pietro.celeste@gmail.com>
  */
-class StreamString
+class StreamString extends Base
 {
-    protected $stream = '';
-    protected $streamLength = 0;
-    protected $position = 0;
-    //Default operation w is necessary for init stream;
-    protected $operations = 'w';
-
-    public function __construct($stream = '', $operations = 'a')
+    public function __construct(string $stream = '', $operations = 'r+')
     {
+        $this->stream = fopen('php://memory', $operations);
         $this->write($stream);
-        $this->operations = $operations;
-    }
-
-    public function eof()
-    {
-        return ($this->position === $this->streamLength);
-    }
-
-    public function isReadable()
-    {
-        return (strpos($this->operations, 'r') !== false || strpos($this->operations, 'a') !== false);
-    }
-
-    public function isSeekable()
-    {
-        return true;
-    }
-
-    public function isWriteable()
-    {
-        return (strpos($this->operations, 'w') !== false || strpos($this->operations, 'a') !== false);
-    }
-
-    public function getContent()
-    {
-        return $this->read($this->streamLength - $this->position);
-    }
-
-    public function read($requestLength)
-    {
-        if ($this->isReadable() === false) {
-            return;
-        }
-        $position = $this->position;
-        $readLength = min($this->streamLength - $position, $requestLength);
-        $this->position += $readLength;
-        return substr($this->stream, $position, $readLength);
-    }
-
-    public function seek($position)
-    {
-        if ($this->isSeekable() === false) {
-            return;
-        }
-        $this->position = min($position, $this->streamLength);
-    }
-
-    public function tell()
-    {
-        return $this->position;
-    }
-
-    public function write($content)
-    {
-        if ($this->isWriteable() === false) {
-            return;
-        }
-        $this->stream .= $content;
-        $this->streamLength = strlen($this->stream);
-    }
-
-    public function __toString()
-    {
-        return $this->stream;
+        $this->rewind();
     }
 }
