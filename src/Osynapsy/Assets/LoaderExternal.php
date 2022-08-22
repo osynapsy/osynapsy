70be46c4e3b4b534a934711a0c4db2f689a70114
Pietro Celeste <p.celeste@osynapsy.net>
2022-08-22 11:45:35 +0200

    Bugfix - introduced glob search of file

--- src/Osynapsy/Assets/LoaderExternal.php
+++ src/Osynapsy/Assets/LoaderExternal.php
@@ -16,16 +16,12 @@ class LoaderExternal extends Loader
     {
         $assetsPathRequest = $this->getParameter(0);
         $namespaceKeySearch = strtok($assetsPathRequest, '/');
-        $this->setNamespacePath($namespaceKeySearch, $assetsPathRequest);
+        $this->setAssetPath($namespaceKeySearch, $assetsPathRequest);
     }
 
-    protected function setNamespacePath($namespaceKeySearch, $assetsPathRequest)
+    protected function setAssetPath($namespaceKeySearch, $assetsPathRequest)
     {
-        $resNamespaces = array_filter(
-            Kernel::getComposer()->getPrefixesPsr4(),
-            function($namespace) use ($namespaceKeySearch) { return sha1($namespace) === $namespaceKeySearch;},
-            ARRAY_FILTER_USE_KEY
-        );
+        $resNamespaces = $this->getPackagePathFromComposer($namespaceKeySearch);
         if (empty($resNamespaces)) {
             return;
         }
@@ -34,7 +30,17 @@ class LoaderExternal extends Loader
         strtok($rawPackagePath, '..'); //$composerPath
         $rootPathSubPackage = strtok('../');
         $packagePath = realpath(substr($rawPackagePath, 0, strpos($rawPackagePath, $rootPathSubPackage) + strlen($rootPathSubPackage)));
-        $this->assetsPath = $packagePath . str_replace($namespaceKeySearch, '', $assetsPathRequest);
+        $files = glob($packagePath . str_replace($namespaceKeySearch, '/*', $assetsPathRequest));
+        $this->assetsPath = !empty($files) ? $files[0] : '';
+    }
+
+    protected function getPackagePathFromComposer($namespaceKeySearch)
+    {
+        return array_filter(
+            Kernel::getComposer()->getPrefixesPsr4(),
+            function($namespace) use ($namespaceKeySearch) { return sha1($namespace) === $namespaceKeySearch;},
+            ARRAY_FILTER_USE_KEY
+        );
     }
 
     public function indexAction()
